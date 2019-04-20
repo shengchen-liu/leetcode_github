70f32086ca0852b5c20ec41ba6805e8662704ad5
lawson1225 <liu.sheng@husky.neu.edu>
2019-04-20 21:52:07 +0000

    Sync LeetCode submission - Graph Valid Tree (python3)

--- problems/graph_valid_tree/solution.py
+++ problems/graph_valid_tree/solution.py
@@ -1,19 +1,34 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n == 1:
-            return True
-        if len(edges) != n - 1:
+        # 1. num of eges == n-1
+        if len(edges) != n-1:
             return False
-        neighbors = {i: [] for i in range(n)}
-        for v, w in edges:
-            neighbors[v] += w,
-            neighbors[w] += v,
+        #graph int:{}
+        # 0: {1, 2, 3}
+        # 1: {0, 4}
+        # 2: {0}
+        # 3: {0}
+        # 4: {1}
+        graph = {i:set() for i in range(n)}
+        for x, y in edges:
+            # x:0, y:1
+            graph[x].add(y)
+            graph[y].add(x)
+        # print(graph)
+        
+        # 2. num of connected nodes == n
+        # BFS
+        # queue
         queue = [0]
-        nodes = set()
-        for v in queue:
-            neighbor_nodes = neighbors.pop(v, [])
-            queue += neighbor_nodes
-            nodes.update(neighbor_nodes)
-
-        # return not neighbors
-        return len(nodes) == n
+        unique_nodes = set()
+        while len(queue) != 0:
+            # loop one line of dic
+            head = queue.pop(0)
+            unique_nodes.add(head)
+            for node in graph[head]:
+                # 1,2,3
+                if node not in unique_nodes:
+                    unique_nodes.add(node)
+                    queue.append(node)
+        return len(unique_nodes) == n
+            
