08f7c8ac9ded3004284f673a97f1a4e015b294e2
lawson1225 <liu.sheng@husky.neu.edu>
2020-01-27 17:40:18 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

---
+++ problems/valid_parentheses/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() == 0) return true;
+        
+        stack<char> par;
+        for (char c : s) {
+            if ( c == '(' || c == '{' || c == '[') {
+                par.push(c);
+            }
+            else { // right
+                if (par.empty()) return false;
+                char last_c = par.top();
+                // check if they can pair
+                if (last_c == '(' && c != ')')  return false;
+                if (last_c == '{' && c != '}')  return false;
+                if (last_c == '[' && c != ']')  return false;
+                par.pop();
+            }
+        }
+        if (par.empty()) return true;
+        else return false;
+        
+    }
+};
