204faad04606477b4449e82b7e5914882d1859d7
lawson1225 <liu.sheng@husky.neu.edu>
2020-02-04 17:15:16 +0000

    Sync LeetCode submission - Last Substring in Lexicographical Order (cpp)

---
+++ problems/last_substring_in_lexicographical_order/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int n = s.size();
+        vector<int> pre(n, 0);
+        int start = 0;
+        int to_match = 0;
+        int visited = -1;
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] > s[start]) {
+                start = i;
+                to_match = start;
+            } 
+            else if (s[to_match] == s[i]) {
+                if (i - start <= 2 * (to_match - start)) {
+                    pre[to_match] = to_match - start;
+                    start = to_match;
+                    to_match = start + 1;
+                }
+                else {
+                    to_match ++;
+                }
+            }
+            else if (s[to_match] > s[i]) {
+                to_match = start;
+            }
+            else if (s[to_match] < s[i]) {
+                start = i - (to_match - start);
+                to_match = start;
+            }
+        }
+        int tot = 0;
+        int pos = start;
+        while (pos > 0) {
+            tot += pre[pos];
+            pos -= pre[pos];
+            if (pre[pos] == 0) break;
+        }
+        return s.substr(start - tot);
+    }
+};
