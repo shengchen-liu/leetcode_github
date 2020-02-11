3eb8cab45d392c0d0dcff7af7a41c0ab604b9ce0
lawson1225 <liu.sheng@husky.neu.edu>
2020-02-11 18:14:51 +0000

    Sync LeetCode submission - Generate Parentheses (cpp)

---
+++ problems/generate_parentheses/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        dfs(n, n, "", res);
+        return res;
+    }
+    void dfs (int left, int right, string out, vector<string> &res) {
+        if (left > right) return;
+        if (left == 0 && right == 0) res.push_back(out);
+        else {
+            if (left > 0) dfs(left - 1, right, out + '(', res);
+            if (right > 0) dfs(left, right - 1, out + ')', res);
+        }
+    }
+};
