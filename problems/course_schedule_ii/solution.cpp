450f541b5e92740c0949aadc9ae7ed21dbf9c774
lawson1225 <liu.sheng@husky.neu.edu>
2020-02-14 21:39:56 +0000

    Sync LeetCode submission - Course Schedule II (cpp)

--- problems/course_schedule_ii/solution.cpp
+++ problems/course_schedule_ii/solution.cpp
@@ -1,48 +1,42 @@
+/**
+[child, parent]
+**/
+
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> in_degree(numCourses); // initial as zeros [0,0,0,...,0]
-        vector<vector<int>> graph(numCourses);
-
-        for(vector edge : prerequisites){
-            in_degree[edge[0]]++;
+        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<int> res;
+        // in degree
+        vector<int> in_degree(numCourses, 0);
+        for (auto edge : prerequisites) {
             graph[edge[1]].push_back(edge[0]);
+            ++in_degree[edge[0]];
         }
-        //[0, 1, ...,]
         
-        //initialize a queue
+        // BFS
         queue<int> q;
-        for(int i = 0; i < numCourses; i++){
-            if(in_degree[i] == 0){
+        for (int i = 0; i < numCourses; ++i) {
+            if (in_degree[i] == 0) {
                 q.push(i);
             }
         }
-        //[0]
         
-        // Build a 2-d vector or map
-        //[[1],
-        // []]
-        
-//         vector<vector<int>> graph(numCourses);
-//         for (vector edge : prerequisites){
-//             graph[edge[1]].push_back(edge[0]);
-//         }
-        
-        vector<int> result;
-        int count = 0;
-        while(!q.empty()){
-            int root = q.front(); // root : 0
+        while (!q.empty()) {
+            int course = q.front();
+            res.push_back(course);
             q.pop();
-            result.push_back(root); //[0]
-            count++;
-            for (int child: graph[root]){
-                in_degree[child]--;
-                if(in_degree[child] == 0) q.push(child);
+            // q's children
+            for (auto child : graph[course]) {
+                --in_degree[child];
+                if (in_degree[child] == 0) q.push(child);
             }
         }
-        if(count!=numCourses){
-            result.clear();
+        
+        for (int i = 0; i < numCourses; ++i) {
+            if (in_degree[i] != 0) return {};
         }
-        return result;
+        return res;
+        
     }
 };
