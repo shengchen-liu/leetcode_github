389c520c6d2a9612b39b89417ac00e3b54ff301a
lawson1225 <liu.sheng@husky.neu.edu>
2020-02-27 00:35:03 +0000

    Sync LeetCode submission - Next Permutation (cpp)

---
+++ problems/next_permutation/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i, j, n = nums.size();
+        for (int i  = n - 2; i >= 0; --i) {
+            if (nums[i + 1] > nums[i]) {
+                for (j = n - 1; j > i; --j) {
+                    if (nums[j] > nums[i]) break;
+                }
+                swap(nums[i], nums[j]);
+                reverse(nums.begin() + i + 1, nums.end());
+                return;
+            }
+        }
+        reverse(nums.begin(), nums.end());
+    }
+};
