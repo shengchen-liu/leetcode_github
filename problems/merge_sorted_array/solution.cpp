eccd9227592f6efec428802987f596b1fc790ca3
lawson1225 <liu.sheng@husky.neu.edu>
2020-03-02 16:00:56 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

---
+++ problems/merge_sorted_array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        
+        // set pointer for nums1
+        int p = m + n - 1;
+        
+        while ((p1 >= 0) && (p2 >= 0)) {
+            // compare two elements, add the larger one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+        
+        // add missing elements from nums2
+        while (p2 >= 0) {
+            nums1[p--] = nums2[p2--];
+        }
+    }
+};
