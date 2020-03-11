7c3d2131cf3fe704eae84374d56a168027483034
lawson1225 <liu.sheng@husky.neu.edu>
2020-03-11 20:03:08 +0000

    Sync LeetCode submission - Valid Palindrome (cpp)

---
+++ problems/valid_palindrome/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1 ;
+        while (left < right) {
+            if (!isalnum(s[left])) ++left;
+            else if (!isalnum(s[right])) --right;
+            else if ((s[left] + 32 - 'a') %32 != (s[right] + 32 - 'a') % 32) return false;
+            else {
+                ++left; --right;
+            }
+        }
+        return true;
+    }
+};
