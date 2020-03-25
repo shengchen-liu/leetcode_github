8d4f43da07eac0e2c2aec280fa55bc53fe6d1135
lawson1225 <liu.sheng@husky.neu.edu>
2020-03-25 03:45:17 +0000

    Sync LeetCode submission - Partition Labels (cpp)

---
+++ problems/partition_labels/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        vector<int> res;
+        int n = S.size();
+        int start = 0;
+        int last = 0;
+        unordered_map<char, int> m;
+        for (int i = 0; i < n; ++i) {
+            m[S[i]] = i;
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            last = max(last, m[S[i]]);
+            if (i == last) {
+                res.push_back(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return res;
+    }
+};
