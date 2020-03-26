ac82f7d3dc9988ef10333bb6f72369d3c42d6993
lawson1225 <liu.sheng@husky.neu.edu>
2020-03-26 00:15:23 +0000

    Sync LeetCode submission - All Nodes Distance K in Binary Tree (cpp)

---
+++ problems/all_nodes_distance_k_in_binary_tree/solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        if (!root) return {};
+        vector<int> res;
+        unordered_map<TreeNode*, TreeNode*> parent;
+        unordered_set<TreeNode*> visited;
+        findParent(root, parent);
+        helper(target, K, parent, visited, res);
+        return res;
+        
+    }
+    
+    void findParent(TreeNode* node, unordered_map<TreeNode*, TreeNode*> &parent) {
+        if (!node) return;
+        if (node->left) parent[node->left] = node;
+        if (node->right) parent[node->right] = node;
+        findParent(node->left, parent);
+        findParent(node->right, parent);
+    }
+    
+    void helper(TreeNode* node, int K, 
+                unordered_map<TreeNode*, TreeNode*> &parent, 
+                unordered_set<TreeNode*> &visited,
+                vector<int> &res) {
+        if (visited.count(node)) return;
+        visited.insert(node);
+        if (K == 0) {
+            res.push_back(node->val);
+            return;
+        }
+        if (node->left) helper(node->left, K - 1, parent, visited, res);
+        if (node->right) helper(node->right, K - 1, parent, visited, res);
+        if (parent[node]) helper(parent[node], K - 1, parent, visited, res);
+    }
+};
