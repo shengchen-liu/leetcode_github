9770e5c9a83e1360b9aa4678a2a83354fd7a909e
lawson1225 <liu.sheng@husky.neu.edu>
2020-03-27 23:29:23 +0000

    Sync LeetCode submission - Binary Tree Vertical Order Traversal (cpp)

---
+++ problems/binary_tree_vertical_order_traversal/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        map<int, vector<int>> m; // treemap
+        queue<pair<int, TreeNode*>> q;
+        q.push({0, root}); // x, node
+        while (!q.empty()) {
+            auto a = q.front();
+            q.pop();
+            m[a.first].push_back(a.second->val);
+            if (a.second->left) q.push({a.first - 1, a.second->left});
+            if (a.second->right) q.push({a.first + 1, a.second->right});
+        }
+        
+        for (auto a : m) {
+            res.push_back(a.second);
+        }
+        return res;
+    }
+};
