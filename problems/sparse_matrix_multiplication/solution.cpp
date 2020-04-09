f045db061a448c045600b948e4b0aa611f28bf7d
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-09 04:00:50 +0000

    Sync LeetCode submission - Sparse Matrix Multiplication (cpp)

---
+++ problems/sparse_matrix_multiplication/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> res(A.size(), vector<int>(B[0].size()));
+        for (int i = 0; i < A.size(); ++i) {
+            for (int k = 0; k < A[0].size(); ++k) {
+                for (int j = 0; j < B[0].size(); ++j) {
+                    if (B[k][j] != 0) res[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return res;
+    }
+};
