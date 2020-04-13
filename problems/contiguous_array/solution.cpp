9838d018c24bce693c5b098aa3e710ba6459cc21
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-13 18:55:10 +0000

    Sync LeetCode submission - Contiguous Array (cpp)

---
+++ problems/contiguous_array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        int sum = 0;
+        unordered_map<int, int> m{{0, -1}};
+        for (int i = 0; i < n; ++i) {
+            sum += (nums[i] == 1) ? 1 : -1;
+            if (m.count(sum)) {
+                res = max(res, i - m[sum]);
+            } else {
+                m[sum] = i;
+            }
+        }
+        return res;
+    }
+};
