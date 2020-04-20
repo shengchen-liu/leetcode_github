90f84d84cad17fbbcec2c2e55ab23f1683be660d
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-20 20:42:18 +0000

    Sync LeetCode submission - Max Area of Island (cpp)

---
+++ problems/max_area_of_island/solution.cpp
@@ -0,0 +1,45 @@
+/**
+BFS
+queue of 1's
+for each 1:
+    find it's neighbor that equals to 1 and is not visited, push to the queue
+**/ 
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int res = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<int> dx {-1, 0, 1, 0};
+        vector<int> dy {0, -1, 0, 1};
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    int cnt = 0;
+                    queue<vector<int>> q;
+                    q.push({i, j});
+                    visited[i][j] = true;
+                    while (!q.empty()) {
+                        auto t = q.front();
+                        q.pop();
+                        int x = t[0];
+                        int y = t[1];
+                        ++cnt;
+                        for (int k = 0; k < 4; ++k) {
+                            int x_new = x + dx[k];
+                            int y_new = y + dy[k];
+                            if (x_new < 0 || x_new >= m || y_new <0 || y_new >= n ||
+                                grid[x_new][y_new] == 0 || visited[x_new][y_new]) 
+                                continue;
+                            visited[x_new][y_new] = true;
+                            q.push({x_new, y_new});
+                        }
+                    }
+                    res = max(res, cnt);
+                }
+            }
+        }
+        return res;
+    }
+};
