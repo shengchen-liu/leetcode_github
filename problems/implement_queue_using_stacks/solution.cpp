61c40bb4f0d8da2ad5a113a38e623eadf8dd7b1b
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-21 00:38:53 +0000

    Sync LeetCode submission - Implement Queue using Stacks (cpp)

--- problems/implement_queue_using_stacks/solution.cpp
+++ problems/implement_queue_using_stacks/solution.cpp
@@ -1,59 +1,38 @@
-/**
-queue: FIFO
-stack: FILO
-[1]
-[1, 2]   stack1: [2, 1]  stack2 : [1, 2]
-[1, 2, 3] s1: [3, 2, 1]  s2: [1, 2, 3]
-[1, 2]: peek() => 1
-[2]
-
-**/
 class MyQueue {
 public:
     /** Initialize your data structure here. */
-    MyQueue() {
-        
-    }
+    MyQueue() {}
     
     /** Push element x to the back of queue. */
     void push(int x) {
-        if (s1.empty()) {
-            front = x;
+        stack<int> tmp;
+        while (!st.empty()) {
+            tmp.push(st.top()); st.pop();
         }
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s2.push(x);
-        while (!s2.empty()) {
-            s1.push(s2.top());
-            s2.pop();
+        st.push(x);
+        while (!tmp.empty()) {
+            st.push(tmp.top()); tmp.pop();
         }
     }
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int val = s1.top();
-        s1.pop();
-        if (!s1.empty()) {
-            front = s1.top();
-        }
+        int val = st.top(); st.pop();
         return val;
     }
     
     /** Get the front element. */
     int peek() {
-        return front;
-        
+        return st.top();
     }
     
     /** Returns whether the queue is empty. */
     bool empty() {
-        return s1.empty();
+        return st.empty();
     }
+    
 private:
-    stack<int> s1, s2;
-    int front;
+    stack<int> st;
 };
 
 /**
