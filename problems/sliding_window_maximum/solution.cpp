bcfe4a821cf519c54d92ce5b9a0ad37c46e6cc17
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-22 21:55:18 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- problems/sliding_window_maximum/solution.cpp
+++ problems/sliding_window_maximum/solution.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-//     1. brute force
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         vector<int> result;
-//         if (nums.empty()) return result; 
-//         for (int i = 0; i < nums.size() - k + 1; i++) {
-//             // find max in the window
-//             int max_num = INT_MIN;
-//             for (int j = i; j < k + i; j++) {
-//                 if (nums[j] > max_num) {
-//                     max_num = nums[j];
-//                 }
-//             }
-//             result.push_back(max_num);
-//         }
-//         return result;
-//     }
-    
-//     2. BST
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
-        multiset<int> st;
+        deque<int> q;
         for (int i = 0; i < nums.size(); ++i) {
-            if (i >= k) st.erase(st.find(nums[i - k]));
-            st.insert(nums[i]);
-            if (i >= k - 1) res.push_back(*st.rbegin());
+            while (!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+            q.push_back(i);
+            
+            // if we have enough elements
+            if (i - k + 1 >= 0) res.push_back(nums[q.front()]);
+            // check if the first element in the window is the max element
+            if (i - k + 1 == q.front()) q.pop_front();
         }
         return res;
-        
     }
 };
