8ddae8ce9c65bb93370b0a6ea676902d0b14045e
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-27 22:34:56 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

--- problems/move_zeroes/solution.cpp
+++ problems/move_zeroes/solution.cpp
@@ -1,13 +1,7 @@
-/**
-[0,1,0,3,12]
-
-**/
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return;
         int i = 0;
         int j = 0;
         while (j < n) {
@@ -16,6 +10,5 @@ public:
             }
             ++j;
         }
-        
     }
 };
