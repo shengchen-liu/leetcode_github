24fdaeb4756794a8852a841fa6ef4864db5e544c
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-27 05:07:35 +0000

    Sync LeetCode submission - Container With Most Water (cpp)

--- problems/container_with_most_water/solution.cpp
+++ problems/container_with_most_water/solution.cpp
@@ -1,26 +1,26 @@
-/**
-p1, p2
-maxArea;
-compute maxArea based on p1, p2
-move the shorter one among p1, p2, update maxArea
-**/
+/*
+n = 9
+1. a1, a9 => 1 * (9 - 1) = 8
+2. a2, a9 => min(a2, a9) * (9 - 2) = 7 * 7 = 49
+3. 2, 8 => min(a2, a8) * (8 - 2) = 3 * 6 = 18
+4. 2, 7 => min(a2, a7) * (7 - 2) = 8 * 5 =  40
+...
+i, j => min(ai, aj) * (j - 1), npdate res
+ai >= aj ? --aj : ++ai;
+
+while (i < j) 
+*/
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int p1 = 0;
-        int p2 = n -1;
-        int maxArea = (p2 - p1) * min(height[p1], height[p2]);
-        
-        while (p1 < p2) {
-            if (height[p1] <= height[p2]) {
-                ++p1;
-            } else {
-                --p2;
-            }
-            int temp = (p2 - p1) * min(height[p1], height[p2]);
-            maxArea = temp > maxArea ? temp : maxArea;
+        int res = 0;
+        int i = 0;
+        int j = height.size() - 1;
+        while (i < j) {
+            int tmp = min(height[i], height[j]) * (j - i);
+            res = max(res, tmp);
+            height[i] >= height[j] ? --j : ++i;
         }
-        return maxArea;
+        return res;
     }
 };
