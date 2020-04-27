cdc2337c72bda611e99637f615f26adc613e7539
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-27 05:42:24 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if (nums.size() == 0) return {};
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result;
-        int n = nums.size();  //6
-        int target = nums[0];
-        for (int i = 0; i < n - 2; ++i) {
-            if (i > 0 && nums[i] == target) continue; // skip duplicats
-            target = nums[i]; // fixed
-            
-            // 2 sum
-            int p1 = i + 1;
-            int p2 = n - 1;
-            while (p1 < p2) {
-                if (nums[p1] + nums[p2] == -target) {
-                    result.push_back({target, nums[p1], nums[p2]});
-                    while (p1 < p2 && nums[p1] == nums[p1 + 1]){
-                        ++p1;
-                    }
-                    while (p1 < p2 && nums[p2] == nums[p2 - 1]){
-                        --p2;
-                    }
-                    ++p1;
-                    --p2;
-                }
-                else if (nums[p1] + nums[p2] > -target) {
-                    --p2;
-                }
-                else {
-                    ++p1;
-                }
-            }
-        }
-        return result;
-        
+    sort(begin(nums), end(nums));
+    vector<vector<int>> res;
+    for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+        if (i == 0 || nums[i - 1] != nums[i])
+            twoSumII(nums, i, res);
+    return res;
+}
+void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
+    int lo = i + 1, hi = nums.size() - 1;
+    while (lo < hi) {
+        int sum = nums[i] + nums[lo] + nums[hi];
+        if (sum < 0 || (lo > i + 1 && nums[lo] == nums[lo - 1]))
+            ++lo;
+        else if (sum > 0 || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1]))
+            --hi;
+        else
+            res.push_back({ nums[i], nums[lo++], nums[hi--]});
     }
+}
 };
