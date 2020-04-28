1593fddc2910cd3d5b0d5dcf0c79efe036702881
lawson1225 <liu.sheng@husky.neu.edu>
2020-04-28 22:49:24 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

--- problems/add_two_numbers/solution.cpp
+++ problems/add_two_numbers/solution.cpp
@@ -9,22 +9,23 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result = new ListNode(-1);  
-        ListNode* cur = result;
-        int carry = 0;
-        while(l1 || l2){
-            int val_1 = l1 ? l1->val : 0;  //2, 4, 3
-            int val_2 = l2 ? l2->val : 0;  //5, 6, 4
-            int digit = val_1 + val_2 + carry;  //7, 10, 8
-            int sum = digit % 10; // 7, 0, 8
-            carry = digit / 10; //0, 1, 0
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
-            cur->next = new ListNode(sum); //[7] [0] [8]
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+        int add = 0;
+        while (l1 || l2) {
+            int a = l1 ? l1->val : 0;
+            int b = l2 ? l2->val : 0;
+            int sum = a + b + add;
+            add = sum / 10;
+            sum = sum % 10;
+            cur->next = new ListNode(sum);
             cur = cur->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
         }
-        if(carry > 0) cur->next = new ListNode(1);
-        return result->next;
+        if (add == 1) {
+            cur->next = new ListNode(1);
+        } 
+        return dummy->next;
     }
-    
 };
