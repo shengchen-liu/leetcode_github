0db45c8545a601e37909e38fe0b91b80adc018c9
lawson1225 <liu.sheng@husky.neu.edu>
2020-05-15 17:48:51 +0000

    Sync LeetCode submission - Remove Linked List Elements (cpp)

--- problems/remove_linked_list_elements/solution.cpp
+++ problems/remove_linked_list_elements/solution.cpp
@@ -3,30 +3,32 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
-pre: last element whose value != val
+/*
+one pass
+two pointers: pre, next
+pre: the last elem whose val is not val
+next: the next elem whose val is not val
 
-while cur->val == val: cur = cur->next
-
-pre->next = cur
-pre = pre->next
-**/
+*/
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* pre = dummy;
+        ListNode* dummy = new ListNode(0);
         dummy->next = head;
-        while (pre->next) {
-            if (pre->next->val == val) {
-                pre->next = pre->next->next; 
+        ListNode* pre = dummy;
+        ListNode* cur = head;
+        while (cur) {
+            if (cur->val != val) {
+                pre = cur;
+            } else {
+                pre->next = cur->next;
             }
-            else {
-                pre = pre->next;
-            }
+            cur = cur->next;
         }
         return dummy->next;
     }
