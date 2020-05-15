94292ffc0b4f5302b223ff50cc82e40efbce48d5
lawson1225 <liu.sheng@husky.neu.edu>
2020-05-15 19:45:56 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (cpp)

--- problems/minimum_depth_of_binary_tree/solution.cpp
+++ problems/minimum_depth_of_binary_tree/solution.cpp
@@ -4,16 +4,35 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+        int l = 1;
         if (!root) return 0;
-        if (root->left && root->right) return 1 + min(minDepth(root->left), minDepth(root->right));
-        if (root->left && !root->right) return 1 + minDepth(root->left);
-        if (root->right && !root->left) return 1 + minDepth(root->right);
-        return 1;
+        // if a node is a leaf, we return its depth
+        // BFS => fisrt node that is a leaf 
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                // check if this node is a leaf
+                if (!node->left && !node->right) return l;
+                else {
+                    if (node->left) q.push(node->left);
+                    if (node->right) q.push(node->right);
+                }
+            }
+            ++l;
+        }
+        return 0;
+        
     }
 };
