eac27b2fd81d8c14fd0e204672018363aa4b27e0
lawson1225 <liu.sheng@husky.neu.edu>
2020-06-16 19:14:48 +0000

    Sync LeetCode submission - Sliding Window Median (cpp)

--- problems/sliding_window_median/solution.cpp
+++ problems/sliding_window_median/solution.cpp
@@ -1,70 +1,33 @@
 class Solution {
 public:
-vector<double> medianSlidingWindow(vector<int>& nums, int k)
-{
-    vector<double> medians;
-    unordered_map<int, int> hash_table;
-    priority_queue<int> lo;                                 // max heap
-    priority_queue<int, vector<int>, greater<int> > hi;     // min heap
-
-    int i = 0;      // index of current incoming element being processed
-
-    // initialize the heaps
-    while (i < k)
-        lo.push(nums[i++]);
-    for (int j = 0; j < k / 2; j++) {
-        hi.push(lo.top());
-        lo.pop();
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        vector<double> res;
+        multiset<int> small, large;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i >= k) {
+                if (small.count(nums[i - k])) small.erase(small.find(nums[i - k]));
+                else if (large.count(nums[i - k])) large.erase(large.find(nums[i - k]));
+            }
+            if (small.size() <= large.size()) {
+                if (large.empty() || nums[i] <= *large.begin()) small.insert(nums[i]);
+                else {
+                    small.insert(*large.begin());
+                    large.erase(large.begin());
+                    large.insert(nums[i]);
+                }
+            } else {
+                if (nums[i] >= *small.rbegin()) large.insert(nums[i]);
+                else {
+                    large.insert(*small.rbegin());
+                    small.erase(--small.end());
+                    small.insert(nums[i]);
+                }
+            }
+            if (i >= (k - 1)) {
+                if (k % 2) res.push_back(*small.rbegin());
+                else res.push_back(((double)*small.rbegin() + *large.begin()) / 2);
+            }
+        }
+        return res;
     }
-
-    while (true) {
-        // get median of current window
-        medians.push_back(k & 1 ? lo.top() : ((double)lo.top() + (double)hi.top()) * 0.5);
-
-        if (i >= nums.size())
-            break;                          // break if all elements processed
-
-        int out_num = nums[i - k],          // outgoing element
-            in_num = nums[i++],             // incoming element
-            balance = 0;                    // balance factor
-
-        // number `out_num` exits window
-        balance += (out_num <= lo.top() ? -1 : 1);
-        hash_table[out_num]++;
-
-        // number `in_num` enters window
-        if (!lo.empty() && in_num <= lo.top()) {
-            balance++;
-            lo.push(in_num);
-        }
-        else {
-            balance--;
-            hi.push(in_num);
-        }
-
-        // re-balance heaps
-        if (balance < 0) {                  // `lo` needs more valid elements
-            lo.push(hi.top());
-            hi.pop();
-            balance++;
-        }
-        if (balance > 0) {                  // `hi` needs more valid elements
-            hi.push(lo.top());
-            lo.pop();
-            balance--;
-        }
-
-        // remove invalid numbers that should be discarded from heap tops
-        while (hash_table[lo.top()]) {
-            hash_table[lo.top()]--;
-            lo.pop();
-        }
-        while (!hi.empty() && hash_table[hi.top()]) {
-            hash_table[hi.top()]--;
-            hi.pop();
-        }
-    }
-
-    return medians;
-}
 };
