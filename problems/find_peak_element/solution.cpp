dc760e5fa070e76d625c6fc8ea23b904e9b47447
lawson1225 <liu.sheng@husky.neu.edu>
2020-06-17 21:57:44 +0000

    Sync LeetCode submission - Find Peak Element (cpp)

--- problems/find_peak_element/solution.cpp
+++ problems/find_peak_element/solution.cpp
@@ -1,35 +1,17 @@
-/*
-    3 
-  2
-1     1
-
-
-          6 
-        5
-            4
-      3
-  2   
-1   1       
-*/
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int start = 0, end = nums.size() - 1;
-        while(start + 1 < end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid] > nums[mid-1]){ // peak on right
-                start = mid;
-            }else{
-                end = mid;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1]) { // peak on the right
+                left = mid;
+            } else {
+                right = mid;
             }
         }
-        
-        if(nums[start] < nums[end]){
-            return end;
-        }else{
-            return start;
-        }
-        
+        if (nums[left] > nums[right]) return left;
+        else return right;
     }
 };
