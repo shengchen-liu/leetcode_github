8968436ca5b100ccc7ab875b5877f8149df32425
lawson1225 <liu.sheng@husky.neu.edu>
2020-06-19 18:01:25 +0000

    Sync LeetCode submission - K Closest Points to Origin (cpp)

--- problems/k_closest_points_to_origin/solution.cpp
+++ problems/k_closest_points_to_origin/solution.cpp
@@ -1,26 +1,23 @@
-
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        priority_queue<vector<int>, vector<vector<int>>, compare> pq; // min heap
-        for (vector<int>& point : points) {
-            pq.push(point);
-            if (pq.size() > K) {
-                pq.pop();
-            }
-        }
-        vector<vector<int>> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top());
-            pq.pop();
-        }
-        return ans;
-    }
+	auto comp = [](const vector<int> &a, const vector<int> &b) {
+		return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1];
+	};
+	priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
+	for (int i = 0; i < points.size(); i++) {
+		pq.push(points[i]);
+		if (pq.size() > K) {
+			pq.pop();
+		}
+	}
 
-private:
-    struct compare {
-        bool operator()(vector<int>& a, vector<int> &b) {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        }
-    };
+	vector<vector<int>> res;
+	while (!pq.empty()) {
+		res.push_back(pq.top());
+		pq.pop();
+	}
+
+	return res;
+}
 };
