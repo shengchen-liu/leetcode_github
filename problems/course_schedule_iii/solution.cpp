49958986a5f2d09f711b65796247a84379a21ae3
lawson1225 <liu.sheng@husky.neu.edu>
2020-06-19 17:10:11 +0000

    Sync LeetCode submission - Course Schedule III (cpp)

---
+++ problems/course_schedule_iii/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int curTime = 0;
+        priority_queue<int> q;
+        sort(courses.begin(), courses.end(), [](vector<int> &a, vector<int> &b){return a[1] < b[1];});
+        for (auto course : courses) {
+            curTime += course[0];
+            q.push(course[0]);
+            if (curTime > course[1]) {
+                curTime -= q.top();
+                q.pop();
+            }
+        }
+        return q.size();
+    }
+};
