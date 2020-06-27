d17b42f063ac9713cd1c9af70c83363f6522e5dc
lawson1225 <liu.sheng@husky.neu.edu>
2020-06-27 15:52:50 +0000

    Sync LeetCode submission - Merge k Sorted Lists (cpp)

--- problems/merge_k_sorted_lists/solution.cpp
+++ problems/merge_k_sorted_lists/solution.cpp
@@ -3,33 +3,34 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    struct compare {
-    bool operator()(const ListNode* l, const ListNode* r) {
-        return l->val > r->val;
-    }
-};
-ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
-    priority_queue<ListNode *, vector<ListNode *>, compare> q;
-    for(auto l : lists) {
-        if(l)  q.push(l);
-    }
-    if(q.empty())  return NULL;
-
-    ListNode* result = q.top();
-    q.pop();
-    if(result->next) q.push(result->next);
-    ListNode* tail = result;            
-    while(!q.empty()) {
-        tail->next = q.top();
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto comp =  [](ListNode* l1, ListNode* l2) {
+            return l1->val > l2->val; 
+        };
+        
+        priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> q(comp); // min heap
+        for (auto l : lists) {
+            if (l) q.push(l);
+        }
+        if (q.empty()) return NULL;
+        
+        ListNode* res = q.top();
         q.pop();
-        tail = tail->next;
-        if(tail->next) q.push(tail->next);
+        if (res->next) q.push(res->next);
+        ListNode *tail = res;
+        while (!q.empty()) {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if (tail->next) q.push(tail->next);
+        }
+        return res;
     }
-    return result;
-}
 };
