0d50af61baaa660b57b7f9a4f513f36cba75ef86
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-01 04:50:53 +0000

    Sync LeetCode submission - Product of Array Except Self (cpp)

--- problems/product_of_array_except_self/solution.cpp
+++ problems/product_of_array_except_self/solution.cpp
@@ -1,31 +1,27 @@
 /*
-1, 2, 3, 4
-left to right, for elem i, find the product up until elem i -1 ([0, i-1])
-arr_l[1, 1, 1*2, 1*2*3]
 
-right to left, for elem i find the product up until elem i -1 ([i + 1, n- 1])
-arr_r[1*4*3*2, 1*4*3, 1*4 , 1]
-
-res: arr_l * arr_r for each ele i
+left[0] = 0
+i = 1 : n
+    left[i] = left[i - 1] * nums[i]
 */
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> p_l(n, 1);
-        vector<int> p_r(n, 1);
         vector<int> res(n, 1);
         
+        int R = 1; // contains the product of all elems to the right
+        
+        // left = res = [1, 1, 1, 1, ..., 1]
         for (int i = 1; i < n; ++i) {
-            p_l[i] = p_l[i - 1] * nums[i - 1];
+            res[i] = res[i - 1] * nums[i - 1];
+            // cout << res[i] << endl;
         }
+        // left: [1, 1, 2, 6]
         
-        for (int i = n - 2; i >= 0; --i) {  // 2
-            p_r[i] = p_r[i + 1] * nums[i + 1];
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            res[i] = p_l[i] * p_r[i];
+        for (int i = n - 1; i >= 0; --i) { 
+            res[i] = res[i] * R; 
+            R *= nums[i];
         }
         return res;
     }
