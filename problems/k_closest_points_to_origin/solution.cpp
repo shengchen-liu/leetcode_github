dd4138c67de60456ee5a842a74556b3e923945a1
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-01 03:50:58 +0000

    Sync LeetCode submission - K Closest Points to Origin (cpp)

--- problems/k_closest_points_to_origin/solution.cpp
+++ problems/k_closest_points_to_origin/solution.cpp
@@ -1,23 +1,33 @@
+/*
+max heap: sort by distance
+*/
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-	auto comp = [](const vector<int> &a, const vector<int> &b) {
-		return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1];
-	};
-	priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
-	for (int i = 0; i < points.size(); i++) {
-		pq.push(points[i]);
-		if (pq.size() > K) {
-			pq.pop();
-		}
-	}
-
-	vector<vector<int>> res;
-	while (!pq.empty()) {
-		res.push_back(pq.top());
-		pq.pop();
-	}
-
-	return res;
-}
+        vector<vector<int>> res;
+        auto cmp = [](vector<int> &p1, vector<int> &p2) {
+            return p1[0] * p1[0] + p1[1] * p1[1] < p2[0] * p2[0] + p2[1] * p2[1];
+        };
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
+        
+        for (int i = 0; i < points.size(); ++i) {
+            if (pq.size() < K) pq.push(points[i]);
+            else if (distance(pq.top()) > distance(points[i])) {
+                pq.pop();
+                pq.push(points[i]);
+            }
+        }
+        
+        while (!pq.empty()) {
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        return res;
+    }
+    
+    int distance(vector<int> point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+    
 };
