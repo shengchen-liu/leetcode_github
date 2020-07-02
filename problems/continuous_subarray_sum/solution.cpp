acc3c778a8143a2a077c6d561609689678f716c9
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-02 22:27:35 +0000

    Sync LeetCode submission - Continuous Subarray Sum (cpp)

--- problems/continuous_subarray_sum/solution.cpp
+++ problems/continuous_subarray_sum/solution.cpp
@@ -1,13 +1,19 @@
+/*
+若数字a和b分别除以数字c，若得到的余数相同，那么(a-b)必定能够整除c。
+
+*/
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        for (int i = 0; i < nums.size(); ++i) {
-            int sum = nums[i];
-            for (int j = i + 1; j < nums.size(); ++j) {
-                sum += nums[j];
-                if (sum == k) return true;
-                if (k != 0 && sum % k == 0) return true;
-            }
+        int n = nums.size();
+        int sum = 0;
+        unordered_map<int, int> m{{0, -1}}; //[余数， 该余数第一次出现的index]
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            int t = (k == 0) ? sum : (sum % k);
+            if (m.count(t)) {
+                if (i - m[t] > 1) return true;
+            } else m[t] = i;
         }
         return false;
     }
