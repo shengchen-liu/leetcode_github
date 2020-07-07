fb74266cd32e4cd2b670d21c724709a8af60832d
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-07 19:26:50 +0000

    Sync LeetCode submission - Diameter of Binary Tree (cpp)

--- problems/diameter_of_binary_tree/solution.cpp
+++ problems/diameter_of_binary_tree/solution.cpp
@@ -4,21 +4,25 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int res = 0;
-        maxDepth(root, res);
-        return res;
+        helper(root, res);
+        return res > 0 ? res - 1 : 0;
     }
-    int maxDepth(TreeNode* node, int& res) {
+    
+    // return the length of the diamter, the path MUST start from the current node
+    int helper(TreeNode* node, int &res) {
         if (!node) return 0;
-        int left = maxDepth(node->left, res);
-        int right = maxDepth(node->right, res);
-        res = max(res, left + right);
-        return max(left, right) + 1;
+        int left = helper(node->left, res);
+        int right = helper(node->right, res);
+        res = max(res, left + right + 1);
+        return 1 + max(left, right);
     }
 };
