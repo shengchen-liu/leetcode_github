4467c724a6c6806765d39d422503902ca4b9c3f5
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-09 23:51:26 +0000

    Sync LeetCode submission - Word Break (cpp)

--- problems/word_break/solution.cpp
+++ problems/word_break/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set wordSet(wordDict.begin(), wordDict.end());
-        queue<int> queue;
-        queue.push(0);
+        unordered_set<string> hashset;
+        for (auto word : wordDict) hashset.insert(word); 
+        //[cats, dog, sand, and, cat]
         vector<bool> visited(s.size(), false);
-        while (!queue.empty()) {
-            int start = queue.front();
-            queue.pop();
+        queue<int> q;
+        q.push(0);
+        while (!q.empty()) {
+            int start = q.front();
+            q.pop();
             if (!visited[start]) {
-                for (int i = start + 1; i < s.size() + 1; ++i) { // i: 1
-                    if (wordSet.count(s.substr(start, i - start))) {
-                        queue.push(i);
-                        if (i == s.size()) {
-                            return true;
-                        }
+                for (int i = start + 1; i < s.size() + 1; ++i) {
+                    if (hashset.count(s.substr(start, i - start))) {
+                        q.push(i);
+                        if (i == s.size()) return true;
                     }
                 }
-                visited[start] = true;                
+                visited[start] = true;
             }
         }
         return false;
-        
     }
-    
 };
