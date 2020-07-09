4d62dba43106fd1474e8521998d317ded2706c13
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-09 23:02:02 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string res = "";
-        unordered_map<char, int> letterCnt;
-        int l = 0;
-        int cnt = 0;
+        int n = s.size();
+        unordered_map<char, int> m;
+        for (auto a : t) ++m[a];
+        int left = 0;
+        int count = 0;
         int minLen = INT_MAX;
-        for (char c : t) ++letterCnt[c];
-        for (int r = 0; r < s.size(); ++r) {
-            if (--letterCnt[s[r]] >= 0) ++cnt;
-            while (cnt == t.size()) {
-                if (minLen > r - l + 1) {
-                    minLen = r - l + 1;
-                    res = s.substr(l, minLen);
+        string res = "";
+        for (int right = 0; right < n; ++right) {
+            if (--m[s[right]] >= 0) ++count;
+            while (count == t.size() ) {
+                if (minLen > right - left + 1) {
+                    minLen = right - left + 1;
+                    res = s.substr(left, minLen);
                 }
-                if (++letterCnt[s[l]] > 0) --cnt;
-                ++l;
+                if (++m[s[left]] > 0) --count;
+                ++left;
             }
         }
         return res;
