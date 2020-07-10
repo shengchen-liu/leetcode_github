eda1110d34ac086cdd6bba3c92c47be6349a43d1
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-10 16:02:59 +0000

    Sync LeetCode submission - Binary Search Tree Iterator (cpp)

--- problems/binary_search_tree_iterator/solution.cpp
+++ problems/binary_search_tree_iterator/solution.cpp
@@ -4,7 +4,9 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class BSTIterator {
@@ -38,6 +40,7 @@ public:
     
 private:
     stack<TreeNode*> s;
+    
 };
 
 /**
