28000fe77da829e3385aeabbc1c4c852f2f4c89d
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-12 23:09:51 +0000

    Sync LeetCode submission - Is Graph Bipartite? (cpp)

--- problems/is_graph_bipartite?/solution.cpp
+++ problems/is_graph_bipartite?/solution.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> root(n);
-        for (int i = 0; i < n; ++i) root[i] = i;
+        vector<int> colors(n);
         for (int i = 0; i < n; ++i) {
-            if (graph[i].empty()) continue;
-            int x = find(root, i);
-            int y = find(root, graph[i][0]);
-            if (x == y) return false;
-            for (int j = 1; j < graph[i].size(); ++j) {
-                int parent = find(root, graph[i][j]);
-                if (x ==  parent) return false;
-                root[parent] = y;
+            if (colors[i] != 0) continue;
+            colors[i] = 1;
+            queue<int> q{{i}}; // node i's neighbor
+            while (!q.empty()) {
+                int t = q.front();
+                q.pop();
+                for (auto a : graph[t]) {
+                    if (colors[a] == colors[t]) return false;
+                    if (colors[a] == 0) {
+                        colors[a] = -1 * colors[t];
+                        q.push(a);
+                    }
+                }
             }
         }
         return true;
-    }
-    
-    int find(vector<int> &root, int i) {
-        return root[i] == i ? i : find(root, root[i]);
     }
 };
