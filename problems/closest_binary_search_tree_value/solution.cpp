decb6a1d65816e55619f161e3fa2a6e1db4a2d13
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-12 21:16:37 +0000

    Sync LeetCode submission - Closest Binary Search Tree Value (cpp)

--- problems/closest_binary_search_tree_value/solution.cpp
+++ problems/closest_binary_search_tree_value/solution.cpp
@@ -4,19 +4,28 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int closestValue(TreeNode* root, double target) {
-        int res = root->val;
-        while (root) {
-            if (abs(res - target) >= abs(root->val - target)) {
-                res = root->val;
-            }
-            root = target < root->val ? root->left : root->right;
-        }
+        int res = 0;
+        double min_dis = DBL_MAX;
+        helper(root, target, min_dis, res);
         return res;
+    }
+    
+    void helper(TreeNode* node, double target, double &min_dis, int &res) {
+        if (!node) return;
+        double tmp = node->val - target;
+        if (abs(tmp) < min_dis) {
+            min_dis = abs(tmp);
+            res = node->val;
+        }
+        if (tmp < 0) helper(node->right, target, min_dis, res);
+        else helper(node->left, target, min_dis, res);
     }
 };
