6c39f831c73da43df16e38fd59bae38c7c46d2e5
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-12 21:45:05 +0000

    Sync LeetCode submission - Divide Two Integers (cpp)

--- problems/divide_two_integers/solution.cpp
+++ problems/divide_two_integers/solution.cpp
@@ -1,15 +1,53 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        long m = labs(dividend), n = labs(divisor), res = 0;
-        if (m < n) return 0;
-        long t = n, p = 1;
-        while (m > (t << 1)) {
-            t <<= 1;
-            p <<= 1;
-        }
-        res += p + divide(m - t, n);
-        if ((dividend < 0) ^ (divisor < 0)) res = -res;
-        return res > INT_MAX ? INT_MAX : res;
+int HALF_INT_MIN = -1073741824;
+
+int divide(int dividend, int divisor) {
+
+    // Special case: overflow.
+    if (dividend == INT_MIN && divisor == -1) {
+        return INT_MAX;
     }
+
+    /* We need to convert both numbers to negatives.
+     * Also, we count the number of negatives signs. */
+    int negatives = 2;
+    if (dividend > 0) {
+        negatives--;
+        dividend = -dividend;
+    }
+    if (divisor > 0) {
+        negatives--;
+        divisor = -divisor;
+    }
+
+    int quotient = 0;
+    /* Once the divisor is bigger than the current dividend,
+     * we can't fit any more copies of the divisor into it. */
+    while (divisor >= dividend) {
+        /* We know it'll fit at least once as divivend >= divisor.
+         * Note: We use a negative powerOfTwo as it's possible we might have
+         * the case divide(INT_MIN, -1). */
+        int powerOfTwo = -1;
+        int value = divisor;
+        /* Check if double the current value is too big. If not, continue doubling.
+        * If it is too big, stop doubling and continue with the next step */
+        while (value >= HALF_INT_MIN && value + value >= dividend) {
+            value += value;
+            powerOfTwo += powerOfTwo;
+        }
+        // We have been able to subtract divisor another powerOfTwo times.
+        quotient += powerOfTwo;
+        // Remove value so far so that we can continue the process with remainder.
+        dividend -= value;
+    }
+
+    /* If there was originally one negative sign, then
+     * the quotient remains negative. Otherwise, switch
+     * it to positive. */
+    if (negatives != 1) {
+        quotient = -quotient;
+    }
+    return quotient;
+}
 };
