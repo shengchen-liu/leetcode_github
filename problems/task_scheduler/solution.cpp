ffa3cecafe1512d38fd290c0d6174829c646a553
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-12 22:34:44 +0000

    Sync LeetCode submission - Task Scheduler (cpp)

--- problems/task_scheduler/solution.cpp
+++ problems/task_scheduler/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int res = 0;
-        int cycle = n + 1;
+        int res = 0, cycle = n + 1;
         unordered_map<char, int> m;
         priority_queue<int> q;
         for (char c : tasks) ++m[c];
         for (auto a : m) q.push(a.second);
-        
         while (!q.empty()) {
             int cnt = 0;
             vector<int> t;
             for (int i = 0; i < cycle; ++i) {
                 if (!q.empty()) {
-                    t.push_back(q.top());
-                    q.pop();
+                    t.push_back(q.top()); q.pop();
                     ++cnt;
                 }
             }
-            
             for (int d : t) {
                 if (--d > 0) q.push(d);
             }
