4315a252ac9add5bafe24befddc3343637b745d1
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-13 21:26:06 +0000

    Sync LeetCode submission - LRU Cache (cpp)

--- problems/lru_cache/solution.cpp
+++ problems/lru_cache/solution.cpp
@@ -6,32 +6,23 @@ public:
     
     int get(int key) {
         auto it = m.find(key);
-        if (it == m.end()) {
-            return -1;
-        }
+        if (it == m.end()) return -1;
         l.splice(l.begin(), l, it->second);
-        //or we can use: l.splice(l.begin(), l, m[key]);
         return it->second->second;
-        
-        
     }
     
     void put(int key, int value) {
         auto it = m.find(key);
-        if (it != m.end()) {
-            l.erase(it->second);
-        }
+        if (it != m.end()) l.erase(it->second);
         l.push_front(make_pair(key, value));
         m[key] = l.begin();
         if (m.size() > cap) {
-            int k = l.rbegin()->first; // key of the last element
+            int k = l.rbegin()->first;
             l.pop_back();
             m.erase(k);
-
         }
-        
     }
-
+    
 private:
     int cap;
     list<pair<int, int>> l;
