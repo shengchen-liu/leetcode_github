d77a4e39a97f8dce515b65826a92c2e58ae58a7a
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-13 22:05:41 +0000

    Sync LeetCode submission - Walls and Gates (cpp)

--- problems/walls_and_gates/solution.cpp
+++ problems/walls_and_gates/solution.cpp
@@ -1,20 +1,45 @@
+/*
+gate to nearest empty room
+BFS
+
+*/
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        queue<pair<int, int>> q;
-        vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-        for (int i = 0; i < rooms.size(); ++i) {
-            for (int j = 0; j < rooms[i].size(); ++j) {
-                if (rooms[i][j] == 0) q.push({i, j});   
-            }
+        queue<vector<int>> q;
+        if (rooms.size() == 0) return;
+        int m = rooms.size();
+        int n = rooms[0].size();
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j){
+                if (rooms[i][j] == 0) {
+                    q.push({i, j});
+                }
+            }                
         }
+        // [0, 2], [3, 0]
+        
+        // BFS
         while (!q.empty()) {
-            int i = q.front().first, j = q.front().second; q.pop();
-            for (int k = 0; k < dirs.size(); ++k) {
-                int x = i + dirs[k][0], y = j + dirs[k][1];
-                if (x < 0 || x >= rooms.size() || y < 0 || y >= rooms[0].size() || rooms[x][y] < rooms[i][j] + 1) continue;
-                rooms[x][y] = rooms[i][j] + 1;
-                q.push({x, y});
+            int size = q.size();
+            for (int k = 0; k < size; ++k) {
+                auto node = q.front();
+                q.pop();
+                int x = node[0];
+                int y = node[1];
+                
+                // check its neighbor
+                for (int i = 0; i < 4; ++i) {
+                    int x_new = x + dx[i];
+                    int y_new = y + dy[i];
+                    if (x_new < 0 || x_new >= m || y_new < 0 || y_new >= n 
+                    || rooms[x_new][y_new] < INT_MAX ) continue;
+                    rooms[x_new][y_new] = rooms[x][y] + 1;
+                    q.push({x_new, y_new});
+                }
             }
         }
     }
