77ffef56fbc0f50dcc2fd65ccefc31bdb611e21d
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-14 00:57:35 +0000

    Sync LeetCode submission - Basic Calculator (cpp)

---
+++ problems/basic_calculator/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0;
+        int num = 0;
+        int sign = 1;
+        int n = s.size();
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            char c = s[i];
+            if (c >= '0') {
+                num = num * 10 + (c - '0');
+            } else if (c == '+' || c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = c == '+' ? 1 : -1;
+            } else if (c == '(') {
+                st.push(res);
+                st.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                res *= st.top(); st.pop();
+                res += st.top(); st.pop();
+            }
+        }
+        res += sign * num;
+        return res;
+    }
+};
