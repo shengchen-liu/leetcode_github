9ebba55e310f66b47456fac2c0ac195efbd5ccb1
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-19 23:29:23 +0000

    Sync LeetCode submission - Copy List with Random Pointer (cpp)

--- problems/copy_list_with_random_pointer/solution.cpp
+++ problems/copy_list_with_random_pointer/solution.cpp
@@ -1,57 +1,27 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // copy next
-        if (!head) return head;
-        
-        Node* curr = head;
-        Node* temp;
-        while (curr){
-            Node* new_curr = new Node(curr->val);
-            temp = curr->next;
-            curr->next = new_curr;
-            new_curr->next = temp;
-            curr = temp;
+        if (!head) return nullptr;
+        Node *cur = head;
+        while (cur) {
+            Node *t = new Node(cur->val, nullptr, nullptr);
+            t->next = cur->next;
+            cur->next = t;
+            cur = t->next;
         }
-        
-        curr = head;
-        while (curr){
-            if (curr->random){ // if random is not pointing to null
-                curr->next->random = curr->random->next;
-            }
-            else{
-                curr->next->random = NULL;
-            }
-            curr = curr->next->next;
+        cur = head;
+        while (cur) {
+            if (cur->random) cur->next->random = cur->random->next;
+            cur = cur->next->next;
         }
-        
-        // break old links
-        curr = head;
-        Node* new_header = head->next;
-        while (curr){
-            Node* new_curr = curr->next;
-            curr->next = new_curr->next;
-            if (new_curr->next){
-                new_curr->next = new_curr->next->next;
-            }
-            curr = curr->next;
+        cur = head;
+        Node *res = head->next;
+        while (cur) {
+            Node *t = cur->next;
+            cur->next = t->next;
+            if (t->next) t->next = t->next->next;
+            cur = cur->next;
         }
-        return new_header;
-        
+        return res;
     }
 };
