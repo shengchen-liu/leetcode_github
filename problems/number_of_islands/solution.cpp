0087a7fa831ec77a90b401241c32c146bbdd17e9
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-19 23:47:37 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,26 +1,35 @@
+// dfs
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), res = 0;
-        vector<vector<bool>> visited(m, vector<bool>(n));
-        vector<int> dirX{-1, 0, 1, 0}, dirY{0, 1, 0, -1};
+        int res = 0;
+        if (grid.size() == 0) return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == '0' || visited[i][j]) continue;
+                helper(grid, visited, i, j, m, n);
                 ++res;
-                queue<int> q{{i * n + j}};
-                while (!q.empty()) {
-                    int t = q.front(); q.pop();
-                    for (int k = 0; k < 4; ++k) {
-                        int x = t / n + dirX[k], y = t % n + dirY[k];
-                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == '0' || visited[x][y]) continue;
-                        visited[x][y] = true;
-                        q.push(x * n + y);
-                    }
-                }
             }
         }
         return res;
+    }
+    
+    void helper(vector<vector<char>>& grid, 
+                vector<vector<bool>>& visited, 
+               int x,
+               int y,
+                int m, 
+                int n) {
+        if (x < 0 || x >= m || y < 0 || y >= n || 
+            visited[x][y] || grid[x][y] == '0') return;
+        visited[x][y] = true;
+        helper(grid, visited, x - 1, y, m, n);
+        helper(grid, visited, x + 1, y, m, n);
+        helper(grid, visited, x, y - 1, m, n);
+        helper(grid, visited, x, y + 1, m, n);
     }
 };
