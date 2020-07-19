7fca4b618322bf0efb1ab8694f0006f44401109d
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-19 00:40:43 +0000

    Sync LeetCode submission - Pow(x, n) (cpp)

--- problems/pow(x,_n)/solution.cpp
+++ problems/pow(x,_n)/solution.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if (n == 0) return 1;
-        double half = myPow(x, n / 2);
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
         if (n % 2 == 0) {
             return half * half;
-        } else if (n > 0){
-            return x * half * half;
         } else {
-            return half * half / x;
+            return half * half * x;
         }
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
     }
 };
