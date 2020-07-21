58b193fc7b4d482da9f93d66bf0c8f9a860bbdbb
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-21 18:26:31 +0000

    Sync LeetCode submission - Vertical Order Traversal of a Binary Tree (cpp)

--- problems/vertical_order_traversal_of_a_binary_tree/solution.cpp
+++ problems/vertical_order_traversal_of_a_binary_tree/solution.cpp
@@ -4,17 +4,16 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/**
-map: [x, map:[y, set(val)]]
-**/
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         map<int, map<int, set<int>>> m;
-        dfs(root, m, 0, 0);
+        helper(root, 0, 0, m);
         vector<vector<int>> res;
         for (auto itx : m) { // itx: pair{int, map<int, set<int>>}
             vector<int> tmp;
@@ -26,10 +25,14 @@ public:
         return res;
     }
     
-    void dfs(TreeNode *node, map<int, map<int, set<int>>> &m, int x, int y) {
+    // dfs
+    void helper(TreeNode* node,
+               int x,
+               int y,
+               map<int, map<int, set<int>>>& m){
         if (!node) return;
         m[x][y].insert(node->val);
-        dfs(node->left, m, x - 1, y + 1);
-        dfs(node->right, m, x + 1, y + 1);
+        helper(node->left, x - 1, y + 1, m);
+        helper(node->right, x + 1, y + 1, m);
     }
 };
