4219cf4dbeebc99436d4d4f99e64b906ecc96211
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-23 17:37:39 +0000

    Sync LeetCode submission - The Maze (cpp)

--- problems/the_maze/solution.cpp
+++ problems/the_maze/solution.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-        if (maze.empty() || maze[0].empty()) return true;
-        int m = maze.size(), n = maze[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
-        queue<pair<int, int>> q;
-        q.push({start[0], start[1]});
+        int m = maze.size();
+        int n = maze[0].size();
+        vector<vector<bool>> visited(m, vector<bool> (n, false));
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        queue<vector<int>> q;
+        q.push(start);
         visited[start[0]][start[1]] = true;
         while (!q.empty()) {
-            auto t = q.front(); q.pop();
-            if (t.first == destination[0] && t.second == destination[1]) return true;
-            for (auto dir : dirs) {
-                int x = t.first, y = t.second;
-                while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == 0) {
-                    x += dir[0]; y += dir[1];
+            auto grid = q.front();
+            q.pop();
+            // check if we have reached the dest
+            if (grid == destination) return true;
+            
+            // check its neighbor
+            for (int i = 0; i < 4; ++i) {
+                // four directions
+                // stop when the reaches to the bundaries or next grid is a wall
+                int x = grid[0];
+                int y = grid[1];
+                while (x >= 0 && x < m && y >= 0 && y < n && 
+                       maze[x][y] == 0) {
+                    x += dx[i];
+                    y += dy[i];
                 }
-                x -= dir[0]; y -= dir[1];
-                if (!visited[x][y]) {
-                    visited[x][y] = true;
-                    q.push({x, y});
-                }
+                x -= dx[i];
+                y -= dy[i];
+                if (visited[x][y]) continue;
+                visited[x][y] = true;
+                q.push({x, y});
             }
         }
         return false;
