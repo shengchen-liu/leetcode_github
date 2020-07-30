60bd4ddeea660cf8394f6caffde7443c7ef2bc23
lawson1225 <liu.sheng@husky.neu.edu>
2020-07-30 23:13:35 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- problems/unique_paths_ii/solution.cpp
+++ problems/unique_paths_ii/solution.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid.empty() || obstacleGrid[0].empty() || obstacleGrid[0][0] == 1) return 0;
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<long>> dp(m + 1, vector<long>(n + 1, 0));
-        dp[0][1] = 1;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (obstacleGrid[i - 1][j - 1] != 0) continue;
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> cur(m, 0);
+        for (int i = 0; i < m; i++) {
+            if (!obstacleGrid[i][0])
+                cur[i] = 1;
+            else break;
         }
-        return dp[m][n];
+        for (int j = 1; j < n; j++) {
+            bool flag = false;
+            if (obstacleGrid[0][j])
+                cur[0] = 0;
+            else flag = true;
+            for (int i = 1; i < m; i++) {
+                if (!obstacleGrid[i][j]) {
+                    cur[i] += cur[i - 1]; 
+                    if (cur[i]) flag = true;
+                }
+                else cur[i] = 0; 
+            }
+            if (!flag) return 0;
+        }
+        return cur[m - 1];
     }
 };
