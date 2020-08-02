c7f45f929d55fac742efca26aec3f23509b3b759
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-02 20:58:22 +0000

    Sync LeetCode submission - Binary Tree Paths (cpp)

--- problems/binary_tree_paths/solution.cpp
+++ problems/binary_tree_paths/solution.cpp
@@ -4,31 +4,28 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// divide and conquer
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> result;
-        if (root == NULL){
-            return {};
-        }
-        // leaf
-        if (root->left == NULL && root->right == NULL) return {to_string(root->val)};
-        
-        vector<string> leftPaths  = binaryTreePaths(root -> left);
-        vector<string> rightPaths  = binaryTreePaths(root -> right);
-        
-        // merge
-        for (string path : leftPaths){
-            result.push_back(to_string(root -> val) + "->" + path);
-        }
-        for (string path : rightPaths){
-            result.push_back(to_string(root -> val) + "->" + path);
-        }
-        return result;
+void binaryTreePaths(vector<string>& result, TreeNode* root, string t) {
+    if(!root->left && !root->right) {
+        result.push_back(t);
+        return;
     }
+
+    if(root->left) binaryTreePaths(result, root->left, t + "->" + to_string(root->left->val));
+    if(root->right) binaryTreePaths(result, root->right, t + "->" + to_string(root->right->val));
+}
+
+vector<string> binaryTreePaths(TreeNode* root) {
+    vector<string> result;
+    if(!root) return result;
+    
+    binaryTreePaths(result, root, to_string(root->val));
+    return result;
+}
 };
