c887ee82c68a3e58ea4bbf1f9420cb99d7e39bc9
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-02 22:43:47 +0000

    Sync LeetCode submission - Kth Smallest Element in a Sorted Matrix (cpp)

--- problems/kth_smallest_element_in_a_sorted_matrix/solution.cpp
+++ problems/kth_smallest_element_in_a_sorted_matrix/solution.cpp
@@ -1,27 +1,25 @@
-/*
-max heap with size of k
-[1]
-[5, 1]
-[9, 5, 1]
-[13, 13, 12, 11, 10, 8, 5, 1]
-[]
-
-*/
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> q;
-        int m = matrix.size();
-        int n = matrix[0].size();
-        for (int i = 0; i < m ; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (q.size() < k) q.push(matrix[i][j]);
-                else if (matrix[i][j] < q.top()) {
-                    q.pop();
-                    q.push(matrix[i][j]);
-                }
+        int left = matrix[0][0], right = matrix.back().back();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt = search_less_equal(matrix, mid);
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+    int search_less_equal(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), i = n - 1, j = 0, res = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= target) {
+                res += i + 1;
+                ++j;
+            } else {
+                --i;
             }
         }
-        return q.top();
+        return res;
     }
 };
