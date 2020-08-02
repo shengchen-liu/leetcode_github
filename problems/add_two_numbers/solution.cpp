0ef6e6eeb3d3945147ec1c84adf3fe4990f8b8f8
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-02 23:35:26 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

--- problems/add_two_numbers/solution.cpp
+++ problems/add_two_numbers/solution.cpp
@@ -3,29 +3,31 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
+        ListNode* dummy = new ListNode();
         ListNode* cur = dummy;
         int add = 0;
         while (l1 || l2) {
             int a = l1 ? l1->val : 0;
-            int b = l2 ? l2->val : 0;
+            int b = l2 ? l2->val : 0;                
             int sum = a + b + add;
             add = sum / 10;
-            sum = sum % 10;
-            cur->next = new ListNode(sum);
+            int rem = sum % 10;
+            cur->next = new ListNode(rem);
             cur = cur->next;
             if (l1) l1 = l1->next;
             if (l2) l2 = l2->next;
         }
         if (add == 1) {
             cur->next = new ListNode(1);
-        } 
+        }
         return dummy->next;
     }
 };
