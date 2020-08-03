9d79f1392a2c58f6f9d589c87ee944f6358847b8
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-03 17:41:15 +0000

    Sync LeetCode submission - Generate Parentheses (cpp)

--- problems/generate_parentheses/solution.cpp
+++ problems/generate_parentheses/solution.cpp
@@ -1,16 +1,40 @@
+/*
+1. # of ( == # of )
+2. at position i, # of ( >= # of )
+    set a cnt >= 0
+n pairs
+1st position: (
+
+dfs (back tracking)
+*/
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        dfs(n, n, "", res);
+        string out;
+        helper(0, n, 0, 0, res, out);
         return res;
     }
-    void dfs (int left, int right, string out, vector<string> &res) {
-        if (left > right) return;
-        if (left == 0 && right == 0) res.push_back(out);
-        else {
-            if (left > 0) dfs(left - 1, right, out + '(', res);
-            if (right > 0) dfs(left, right - 1, out + ')', res);
+    
+    // at position i, generate current string : out
+    void helper(int i, 
+                int n,
+                int left,
+                int right,
+                vector<string>& res, 
+                string out) {
+        if (i == 2 * n) {
+            res.push_back(out); // i : 0, 1
+            return;
+        }
+        
+        // out + '('
+        if (left < n) {
+            helper(i + 1, n, left + 1, right, res, out + '(');
+        }
+        // out + ')'
+        if (left > right) {
+            helper(i + 1, n, left, right + 1, res, out + ')');
         }
     }
 };
