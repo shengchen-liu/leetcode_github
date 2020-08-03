971b17a85c59eb5ccfb28f0ae2e07be32e7222d5
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-03 00:54:13 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- problems/jump_game/solution.cpp
+++ problems/jump_game/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), 0);
+        vector<int> dp(nums.size(), 0); // 当前位置剩余的跳力
         for (int i = 1; i < nums.size(); ++i) {
-            dp[i] = max(dp[i - 1], nums[i - 1]) - 1;
+            dp[i] = max(dp[i - 1], nums[i - 1]) -1;
             if (dp[i] < 0) return false;
         }
         return true;
