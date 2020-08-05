08a37001e1a66490f4d2804bb2569fb8d93fbcd8
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-05 21:16:55 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- problems/decode_ways/solution.cpp
+++ problems/decode_ways/solution.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if (s.empty() || s[0] == '0') return 0;
-        int a = 1, b = 1, n = s.size();
-        for (int i = 1; i < n; ++i) {
-            if (s[i] == '0') a = 0;
-            if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] <= '6')) {
-                a = a + b;
-                b = a - b;
-            } else {
-                b = a;
+        int n = s.size();
+        if (n == 0) return 0;
+        vector<int> dp(n + 1);
+        // int num = (s[0] - '0') + (s[1] - '0'); 
+        dp[0] = 1;
+        dp[1] = (s[0] - '0' > 0) ? 1 : 0;
+        
+        for (int i = 2; i <= n; ++i) {
+            // i'th element in s => s[i - 1]
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            
+            int num = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+            if (num <= 26 && num >= 10) {
+                dp[i] += dp[i - 2];
             }
         }
-        return a;
+        
+        return dp[n];
     }
 };
