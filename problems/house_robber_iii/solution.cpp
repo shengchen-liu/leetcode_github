a1cc9e369195244a9394b79a9df0608a8c3aae2f
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-09 22:11:08 +0000

    Sync LeetCode submission - House Robber III (cpp)

--- problems/house_robber_iii/solution.cpp
+++ problems/house_robber_iii/solution.cpp
@@ -4,10 +4,11 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     int rob(TreeNode* root) {
