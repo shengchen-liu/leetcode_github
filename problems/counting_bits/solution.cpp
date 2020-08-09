90995fb52e952dcf1ff0c11e9dc48a8f4a13d85e
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-09 17:35:03 +0000

    Sync LeetCode submission - Counting Bits (cpp)

--- problems/counting_bits/solution.cpp
+++ problems/counting_bits/solution.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        vector<int> ret(num+1, 0);
-        for (int i = 1; i <= num; ++i)
-            ret[i] = ret[i&(i-1)] + 1;
-        return ret;
+        vector<int> dp(num + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= num; ++i) {
+            dp[i] = dp[i >> 1] + (i & 1); // x / 2 is x >> 1 and x % 2 is x & 1
+        }
+        return dp;
     }
 };
