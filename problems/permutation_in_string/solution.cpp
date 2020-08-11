127e051c5cf83acb1d20bc258178815f131f137c
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-11 19:43:00 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- problems/permutation_in_string/solution.cpp
+++ problems/permutation_in_string/solution.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> m2(26, 0);
         int k = s1.size();
         
-        for (int i = 0; i < s1.size(); ++i) {
+        for (int i = 0; i < k; ++i) {
             ++m1[s1[i] - 'a'];
             ++m2[s2[i] - 'a'];
         }
@@ -16,18 +16,20 @@ public:
             if (m1[i] == m2[i]) ++count;
         }
         
-        // fixed window size
+        // fixed window size: k
         for (int i = 0; i < s2.size() - k; ++i) {
             if (count == 26) return true;
+            // sliding right
             int r = s2[i + k] - 'a';
             int l = s2[i] - 'a';
             ++m2[r];
-            if (m2[r] == m1[r]) ++ count;
-            else if (m2[r] == m1[r] + 1) --count;
-           
+            if (m1[r] == m2[r]) ++count;
+            else if (m1[r] + 1 == m2[r]) --count;
+            
+            // left
             --m2[l];
-            if (m2[l] == m1[l]) ++count;
-            else if (m2[l] == m1[l] - 1) --count;
+            if (m1[l] == m2[l]) ++count;
+            else if (m1[l] - 1 == m2[l]) --count;
         }
         return count == 26;
     }
