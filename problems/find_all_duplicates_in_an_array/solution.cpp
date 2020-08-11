73829fc83ec0baf8f228a20dcdc568c48f7714d4
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-11 22:26:13 +0000

    Sync LeetCode submission - Find All Duplicates in an Array (cpp)

---
+++ problems/find_all_duplicates_in_an_array/solution.cpp
@@ -0,0 +1,13 @@
+// 这类问题的核心是就是找nums[i]和nums[nums[i] - 1]的关系，我们的做法是，对于每个nums[i]，我们将其对应的nums[nums[i] - 1]取相反数，如果其已经是负数了，说明之前存在过，我们将其加入结果res中即可，
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] < 0) res.push_back(idx + 1);
+            nums[idx] = -nums[idx];
+        }
+        return res;
+    }
+};
