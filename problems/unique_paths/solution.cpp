404f0c9f98605ae32007678d4173c2e8bc805c2d
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-16 18:46:51 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> cur(n, 1);
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                cur[j] += cur[j - 1];
+        vector<int> cur(n, 0);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) cur[j] = 1;
+                else {
+                    cur[j] = cur[j] + cur[j - 1];
+                }
             }
         }
         return cur[n - 1];
