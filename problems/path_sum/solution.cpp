c77761287b1471de6ce5abd4530b17d24228317b
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-21 05:42:42 +0000

    Sync LeetCode submission - Path Sum (cpp)

--- problems/path_sum/solution.cpp
+++ problems/path_sum/solution.cpp
@@ -4,17 +4,24 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
         if (!root) return false;
-        sum = sum - root->val;
-        if (!root->left && !root->right){
-            if (sum == 0) return true;
+        // if root is a leaf
+        if (!root->left && !root->right) {
+            if (root->val == sum) return true;
+            else return false;
         }
-        return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+        if (hasPathSum(root->left, sum - root->val) ||
+            hasPathSum(root->right, sum - root->val))
+            return true;
+        return false;
+        
     }
 };
