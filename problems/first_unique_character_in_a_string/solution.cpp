09ae41d834624727512f5ae93f81205924bca7bd
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-26 22:32:10 +0000

    Sync LeetCode submission - First Unique Character in a String (cpp)

--- problems/first_unique_character_in_a_string/solution.cpp
+++ problems/first_unique_character_in_a_string/solution.cpp
@@ -1,28 +1,14 @@
-/**
-hashset
-l:1
-e:3
-t:1
-c:1
-o:1
-d:1
-
-
-**/
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> hashmap;
+        unordered_map<char, int> m;
         for (char c : s) {
-            if (!hashmap.count(c)) hashmap[c] = 1;
-            else {
-                ++hashmap[c];
-            }
+            ++m[c];
         }
-        
         for (int i = 0; i < s.size(); ++i) {
-            if (hashmap[s[i]] == 1) return i;
+            if (m[s[i]] == 1) return i;
         }
         return -1;
+        
     }
 };
