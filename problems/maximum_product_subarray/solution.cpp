f488937c591ecac5d76ba352cdbf825cfb8e6e17
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-30 16:26:23 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- problems/maximum_product_subarray/solution.cpp
+++ problems/maximum_product_subarray/solution.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        
-        // initial
-        int cur_max = nums[0];
-        int cur_min = nums[0];
-        int res = nums[0];
-        
-        for (int i = 1; i < n; ++i) {
-            int temp_max = max(nums[i], max(nums[i] * cur_max, nums[i] * cur_min));
-            cur_min = min(nums[i], min(nums[i] * cur_max, nums[i] * cur_min));
-            cur_max = temp_max;
-            res = max(res, cur_max);
+        int res = INT_MIN;
+        int max_so_far = 1;
+        int min_so_far = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            int cur = nums[i - 1];
+            // max_dp[i] = max(nums[i - 1], max(max_dp[i - 1] * nums[i - 1],
+            //                                 min_dp[i - 1] * nums[i - 1]));
+            // min_dp[i] = min(nums[i - 1], min(max_dp[i - 1] * nums[i - 1],
+            //                                 min_dp[i - 1] * nums[i - 1]));
+            int temp_max = max(cur, max(max_so_far * cur, min_so_far * cur));
+            min_so_far = min(cur, min(max_so_far * cur, min_so_far * cur));
+            
+            max_so_far = temp_max;
+            res = max(max_so_far, res);
         }
         
-        return res;
         
+        return res;
     }
 };
