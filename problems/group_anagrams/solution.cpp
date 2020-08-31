c844a0214c61879f6717240f23d8f47657758bc7
lawson1225 <liu.sheng@husky.neu.edu>
2020-08-31 02:57:20 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- problems/group_anagrams/solution.cpp
+++ problems/group_anagrams/solution.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, vector<string>> hashmap;
+        unordered_map<string, vector<string>> m;
         
-        for (string word : strs) {
-            vector<int> count(26);
+        for (auto word : strs) {
+            vector<int> cnt(26);
             string t;
             for (char c : word) {
-                ++count[c - 'a'];
+                ++cnt[c - 'a'];
             }
+            
             for (int i = 0; i < 26; ++i) {
-                if (count[i] > 0) {
-                    t += string(1, i + 'a') + to_string(count[i]);
+                if (cnt[i] > 0) {
+                    t += string(1, i + 'a') + to_string(cnt[i]);
                 }
             }
-            hashmap[t].push_back(word);
+            m[t].push_back(word);
         }
         
-        for (auto a : hashmap) {
+        for (auto a : m) {
             res.push_back(a.second);
         }
-        
         return res;
-        
     }
 };
