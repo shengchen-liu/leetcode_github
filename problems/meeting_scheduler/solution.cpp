08637c8d0bb6472b9b7c9fe9675221bb1ae899f7
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-07 06:54:05 +0000

    Sync LeetCode submission - Meeting Scheduler (cpp)

---
+++ problems/meeting_scheduler/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        for (auto s : slots1) {
+        if (s[1] - s[0] >= duration) q.push({ s[0], s[1] });
+    }
+    for (auto s : slots2) {
+        if (s[1] - s[0] >= duration) q.push({ s[0], s[1] });
+    }
+    while (q.size() > 1) {
+        auto end1 = q.top().second;
+        q.pop();
+        auto start2 = q.top().first;
+        if (end1 >= start2 + duration)
+            return { start2, start2 + duration };
+    }
+    return {};
+    }
+};
