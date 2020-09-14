0b15209b9d63cdb5d9e9280eb84d8119c6c23d61
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-14 21:31:47 +0000

    Sync LeetCode submission - Basic Calculator II (cpp)

--- problems/basic_calculator_ii/solution.cpp
+++ problems/basic_calculator_ii/solution.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     int calculate(string s) {
-        long res = 0, num = 0, n = s.size();
-        char op = '+';
+        int res = 0;
+        int n = s.size();
         stack<int> st;
+        char oper = '+';
+        int num = 0;
+        
         for (int i = 0; i < n; ++i) {
-            if (s[i] >= '0') {
-                num = num * 10 + s[i] -'0'; 
-            }
-            if ((s[i] < '0' && s[i] != ' ') || i == n - 1) {
-                if (op == '+') st.push(num);
-                if (op == '-') st.push(-num);
-                if (op == '*' || op == '/') {
-                    int tmp = (op == '*') ? st.top() * num : st.top() / num;
+            // cout << num << endl;
+            char c = s[i];
+            // 1. digit
+            if (c - '0' >= 0 && c - '9' <= 0) {
+                num = num * 10 + (c - '0');
+            } 
+            if (c != ' ' && !(c - '0' >= 0 && c - '9' <= 0) || i == n - 1) {
+                if (oper == '+'){
+                    st.push(num);
+                } else if (oper == '-') {
+                    st.push(-num);
+                } else if (oper == '*') {
+                    int tmp = num * st.top();
+                    st.pop();
+                    st.push(tmp);
+                } else if (oper == '/') {
+                    int tmp = st.top() /  num;
                     st.pop();
                     st.push(tmp);
                 }
-                op = s[i];
                 num = 0;
-            }
+                oper = c;
+            } 
         }
         
         while (!st.empty()) {
             res += st.top();
             st.pop();
         }
+        
         return res;
-    } 
+    }
 };
