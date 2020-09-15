8dbf4bb710abd4ebcd98ee85ad3672da425e0549
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-15 05:04:33 +0000

    Sync LeetCode submission - Read N Characters Given Read4 II - Call multiple times (cpp)

---
+++ problems/read_n_characters_given_read4_ii_-_call_multiple_times/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        for (int i = 0; i < n; ++i) {
+            if (readPos == writePos) {
+                writePos = read4(buff);
+                readPos = 0;
+                if (writePos == 0) return i;
+            }
+            buf[i] = buff[readPos++];
+        }
+        return n;
+    }
+    
+private:
+    int readPos = 0, writePos = 0;
+    char buff[4];
+};
