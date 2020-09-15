bbf180926a259e4475d5dd5d3e23f5b54e06316b
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-15 04:50:01 +0000

    Sync LeetCode submission - Read N Characters Given Read4 (cpp)

---
+++ problems/read_n_characters_given_read4/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int res = 0;
+        
+        int copiedChars = 0;
+        int readChars = 4;
+        
+        while (copiedChars < n && readChars == 4) {
+            readChars = read4(buf + copiedChars);
+            copiedChars += readChars;
+        }
+        return min(n, copiedChars);
+    }
+};
