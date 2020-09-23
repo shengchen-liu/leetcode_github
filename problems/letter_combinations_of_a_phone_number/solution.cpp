180bb64cbe0f579240564996385b74e9c07c6f02
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-23 22:53:03 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,29 +1,52 @@
-/**
-DFS
-Duplicates 
-**/
+/*
+2, 3, 4
+2: a, b, c
+3: d, e, f
+4: g, h ,i
+
+0(2):          a,       b,        c
+1(3):     d,         e,        f       d, e, f   d, e, f
+2(4): [g, h, i] [g, h, i]  [g, h, i] ....
+*/
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+        if (digits.size() == 0) return {};
+        unordered_map<int, vector<char>> m;
+        m[2] = {'a', 'b', 'c'};
+        m[3] = {'d', 'e', 'f'};
+        m[4] = {'g', 'h', 'i'};
+        m[5] = {'j', 'k', 'l'};
+        m[6] = {'m', 'n', 'o'};
+        m[7] = {'p', 'q', 'r', 's'};
+        m[8] = {'t', 'u', 'v'};
+        m[9] = {'w', 'x', 'y', 'z'};
         vector<string> res;
-        vector<string> dict {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        dfs(digits, dict, 0, "", res);
+        string path = "";
+        helper(res, path, digits, 0, m);
         return res;
+        
     }
     
-    void dfs(string& digits,  
-             vector<string>& dict, 
-             int level, 
-             string out, 
-             vector<string>& res) {
-        if (level == digits.size()) {
-            res.push_back(out);
+    // at current level, constrcut combinations
+    void helper(vector<string> &res, 
+               string path,
+                string digits,
+               int level,
+               unordered_map<int, vector<char>> &m) {
+        // termination
+        int n = digits.size();
+        if (level == n) {
+            res.push_back(path);
             return;
         }
-        string str = dict[digits[level] - '0'];
-        for (int i = 0; i < str.size(); ++i) {
-            dfs(digits, dict, level + 1, out + str[i], res);
+        
+        // recursion
+        // at current level, go throught all candi
+        int cur = digits[level] - '0';// 2
+        vector<char> cand = m[cur]; //[a, b, c]
+        for (int i = 0; i < cand.size(); ++i) {
+            helper(res, path + cand[i], digits, level + 1, m); //
         }
     }
 };
