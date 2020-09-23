fd2e5a48d3a4d0de2186514a1660b226ca18910b
lawson1225 <liu.sheng@husky.neu.edu>
2020-09-23 21:48:52 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,27 +1,42 @@
+/*
+ [2,3,6,7]
+ 1. sort 
+ 2. backtracking
+ 
+ 0:                          2,                             3, 6, 7
+ 1:               2[4]              3[5],  6[8x], 7[x]
+ 2:          2[6], 3[7Y],           3[8x] 
+ 3: 2[8x]
+*/
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<int> out;
         vector<vector<int>> res;
-        helper(candidates, target, res, 0, out);
+        vector<int> path;
+        helper(res, path, candidates, target, 0);
         return res;
     }
     
-    // from index start, check candidates from start to end can sum up to target 
-    void helper(vector<int>& candidates, 
+    // backtracking
+    void helper(vector<vector<int>> &res, 
+                vector<int>& path, 
+                vector<int>& candidates,
                 int target,
-                vector<vector<int>> &res, 
-                int start,
-               vector<int>& out) {
+                int layer) {
+        // termination
+        int n = candidates.size();
         if (target < 0) return;
-        if (target == 0) res.push_back(out);
-        for (int i = start; i < candidates.size(); ++i) {
-            if (candidates[i] > target) break;
-            out.push_back(candidates[i]);
-            helper(candidates, target - candidates[i], res, i, out);
-            out.pop_back();
+        if (target == 0) {
+            res.push_back(path);
+            return;
         }
         
+        // recursing
+        // we are at current layer
+        for (int i = layer; i < candidates.size(); ++i) {
+            path.push_back(candidates[i]);
+            helper(res, path, candidates, target - candidates[i], i);
+            path.pop_back();
+        }
     }
 };
