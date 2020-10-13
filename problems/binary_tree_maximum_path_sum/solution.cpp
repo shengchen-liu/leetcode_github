7e63470932c7a6e8b7cffa1322a465874b5685e0
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-13 04:06:19 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (cpp)

--- problems/binary_tree_maximum_path_sum/solution.cpp
+++ problems/binary_tree_maximum_path_sum/solution.cpp
@@ -8,21 +8,34 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
+ 
+ helper: return the max path sum, the path must start from the current node
+ 1. if a node is empty: null
+ 2. a node is a leaf: node->val
+ 3. node has left and right:
+    (1) pathL
+    (2) pathR
+    res = max(res, pathL + node->val + pathR)
+    return max(pathL, pathR) + node->al
+ 
  */
+
+
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
         int res = INT_MIN;
-        int sum = helper(root, res);
+        helper(root, res);
         return res;
     }
     
-    // path sum that must include node
+private:
+//    return max path sum, the path must start from the current node
     int helper(TreeNode* node, int &res) {
-        if (!node) return 0;
-        int left = max(helper(node->left, res), 0);
-        int right = max(helper(node->right, res), 0);
-        res = max(res, node->val + left + right);
-        return node->val + max(left, right);
+        if(!node) return 0;
+        int pathL = max(helper(node->left, res), 0);
+        int pathR = max(helper(node->right, res), 0);
+        res = max(res, pathL + pathR + node->val);
+        return max(pathL, pathR) + node->val;
     }
 };
