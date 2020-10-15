3d38cf45d1c9f9df5067e1cb8fffe96dc7aa8b6a
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-15 15:36:31 +0000

    Sync LeetCode submission - Number of Distinct Islands (cpp)

--- problems/number_of_distinct_islands/solution.cpp
+++ problems/number_of_distinct_islands/solution.cpp
@@ -1,31 +1,55 @@
+/*
+dfs
+11011
+10000
+00001
+11011
+
+*/
 class Solution {
 public:
     vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
 
     int numDistinctIslands(vector<vector<int>>& grid) {
+        if (grid.size() == 0) return 0;
+        
         int m = grid.size();
         int n = grid[0].size();
+        
         set<vector<pair<int, int>>> res;
+
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] != 1) continue;
-                vector<pair<int, int>> v;
-                queue<pair<int, int>> q{{{i, j}}};
-                while (!q.empty()) {
-                    auto t = q.front();
-                    q.pop();
-                    for (auto dir : dirs) {
-                        int x = t.first + dir[0];
-                        int y = t.second + dir[1];
-                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] <= 0) continue;
-                        q.push({x, y});
-                        grid[x][y] *= -1;
-                        v.push_back({x - i, y - j});
-                    }
-                }
-                res.insert(v);
+                // dfs
+                vector<pair<int, int>> isl;
+                dfs(i, j, i, j, grid, isl);
+                res.insert(isl);
+                
             }
         }
         return res.size();
+        
+    }
+    
+    void dfs(int x, int y, int x0, int y0,
+             vector<vector<int>>& grid, 
+             vector<pair<int, int>>& isl) {
+        int m = grid.size();
+        int n = grid[0].size(); 
+        
+        // termination
+        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != 1)
+            return;
+        
+        // recursion
+        // curent grid
+        isl.push_back({x - x0, y - y0});
+        grid[x][y] *= -1;
+        
+        // 4 neighbors
+        for (int i = 0; i < 4; ++i) {
+            dfs(x + dirs[i][0], y + dirs[i][1], x0, y0, grid, isl);
+        }
     }
 };
