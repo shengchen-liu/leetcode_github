611ea0c87976c373b90bfde2b37cc6c02148f1c4
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-16 00:39:45 +0000

    Sync LeetCode submission - Top K Frequent Elements (cpp)

--- problems/top_k_frequent_elements/solution.cpp
+++ problems/top_k_frequent_elements/solution.cpp
@@ -1,14 +1,35 @@
+/*
+1,1,1,2,2,3
+
+min heap
+1: 1
+1: 2
+1: 3
+2: 1, 1: 3
+2: 2, 1: 3
+3: 1, 2: 2, 1: 3 size > k => pop the top 
+*/
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
-        priority_queue<pair<int, int>> q;
         vector<int> res;
-        for (auto a : nums) ++m[a];
-        for (auto it : m) q.push({it.second, it.first});
-        for (int i = 0; i < k; ++i) {
-            res.push_back(q.top().second); q.pop();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // min heap
+        unordered_map<int, int> m;
+        for (int num : nums) {
+            m[num]++;
         }
+        
+        // pq
+        for (auto it : m) {
+            pq.push({it.second, it.first});
+            if (pq.size() > k) pq.pop();
+        }
+        
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        
         return res;
     }
 };
