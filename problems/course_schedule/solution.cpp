d13f4cc5de9df018624cd6f79d96fd567a47f0fa
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-16 02:35:16 +0000

    Sync LeetCode submission - Course Schedule (cpp)

--- problems/course_schedule/solution.cpp
+++ problems/course_schedule/solution.cpp
@@ -1,26 +1,59 @@
+/*
+topological sort
+[[1,0], [0, 2], [3, 2], [3, 4]]
+
+0 -> 1
+2 -> 0
+2 -> 3
+4 -> 3
+
+build a in-dgree graph
+1: 1
+0: 1
+3: 2
+2: 0
+4: 0
+
+BFS:
+start with in-dgree == 0 nodes
+update in-dgree graph when a node is visited
+
+end of loop
+compare count == n?
+*/
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses, vector<int>());
-        vector<int> in(numCourses);
-        for (auto a : prerequisites) {
-            graph[a[1]].push_back(a[0]);
-            ++in[a[0]];
+        vector<int> in_degree(numCourses, 0);
+        unordered_map<int, vector<int>> m;
+        // in-degree graph
+        for (auto a : prerequisites){
+            int first = a[0];
+            int second = a[1];
+            in_degree[first]++;
+            m[second].push_back(first);
         }
+        
+        // bfs
         queue<int> q;
         for (int i = 0; i < numCourses; ++i) {
-            if (in[i] == 0) q.push(i);
+            if (in_degree[i] == 0) q.push(i);
         }
+        
+        // q: {2, 4}
+        int cnt = 0;
         while (!q.empty()) {
-            int t = q.front(); q.pop();
-            for (auto a : graph[t]) {
-                --in[a];
-                if (in[a] == 0) q.push(a);
+            int cur = q.front();
+            q.pop();
+            ++cnt;
+            // find all neighbors that cur points to
+            for (int i : m[cur]) {
+                --in_degree[i];
+                if (in_degree[i] == 0)
+                    q.push(i);
             }
         }
-        for (int i = 0; i < numCourses; ++i) {
-            if (in[i] != 0) return false;
-        }
-        return true;
+        return cnt == numCourses;
+        
     }
 };
