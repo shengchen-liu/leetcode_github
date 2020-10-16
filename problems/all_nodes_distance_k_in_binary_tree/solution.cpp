3b19c506f002a679a7ed61394f0fbb76cb6b2862
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-16 14:32:28 +0000

    Sync LeetCode submission - All Nodes Distance K in Binary Tree (cpp)

--- problems/all_nodes_distance_k_in_binary_tree/solution.cpp
+++ problems/all_nodes_distance_k_in_binary_tree/solution.cpp
@@ -6,39 +6,67 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
+ 
+1. find target node
+    dfs
+2. find nodes that has distance K from target's subtree
+    findDown(node, K): dfs
+3. find nodes upward
+    findUp(node, K)
+    
+hashmap node: parent
+3 : null
+5 : 3
+6 : 5
+2 : 5
+7 : 2
+4 : 1
+1 : 3
+0 : 1
+8 : 1
  */
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
-        vector<int> res;
         unordered_map<TreeNode*, TreeNode*> parent;
         unordered_set<TreeNode*> visited;
-        findParent(root, parent);
-        helper(target, K, parent, visited, res);
+        findParent(root, NULL, parent);
+        vector<int> res;
+        helper(target, K, parent, visited, res); // root := 5, k:=2
         return res;
     }
     
-    void findParent(TreeNode* node, unordered_map<TreeNode*, TreeNode*>& parent){
+    void findParent(TreeNode* node, TreeNode* up, unordered_map<TreeNode*, TreeNode*>& parent) {
+        // termination
         if (!node) return;
-        if (node->left) parent[node->left] = node;
-        if (node->right) parent[node->right] = node;
-        findParent(node->left, parent);
-        findParent(node->right, parent);
+        
+        // recursion
+        parent[node] = up;
+        // left child
+        findParent(node->left, node, parent);
+        // right child
+        findParent(node->right, node, parent);
     }
     
     void helper(TreeNode* node, 
-                int K, 
+                int k, 
                 unordered_map<TreeNode*, TreeNode*>& parent,
-                unordered_set<TreeNode*>& visited,
-                vector<int> &res) {
+                unordered_set<TreeNode*> &visited,
+                vector<int>& res) {
+        // termination
+        if (!node) return;
         if (visited.count(node)) return;
         visited.insert(node);
-        if (K == 0) {
+        if (k == 0) {
             res.push_back(node->val);
             return;
         }
-        if (node->left) helper(node->left, K - 1, parent, visited, res);
-        if (node->right) helper(node->right, K - 1, parent, visited, res);
-        if (parent[node]) helper(parent[node], K - 1, parent, visited, res);
+        
+        // recursion
+        helper(node->left, k - 1, parent, visited, res); // 
+        helper(node->right, k - 1, parent, visited, res);
+        helper(parent[node], k - 1, parent, visited, res);
+
+
     }
 };
