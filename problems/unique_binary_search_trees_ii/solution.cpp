d472f561b2e48c727428d0691e6a7b3d54844d76
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-21 05:25:07 +0000

    Sync LeetCode submission - Unique Binary Search Trees II (cpp)

---
+++ problems/unique_binary_search_trees_ii/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        vector<vector<vector<TreeNode*>>> memo(n, vector<vector<TreeNode*>>(n));
+        return helper(1, n, memo);
+    }
+    vector<TreeNode*> helper(int start, int end, vector<vector<vector<TreeNode*>>>& memo) {
+        if (start > end) return {nullptr};
+        if (!memo[start - 1][end - 1].empty()) return memo[start - 1][end - 1];
+        vector<TreeNode*> res;
+        for (int i = start; i <= end; ++i) {
+            auto left = helper(start, i - 1, memo), right = helper(i + 1, end, memo);
+            for (auto a : left) {
+                for (auto b : right) {
+                    TreeNode *node = new TreeNode(i);
+                    node->left = a;
+                    node->right = b;
+                    res.push_back(node);
+                }
+            }
+        }
+        return memo[start - 1][end - 1] = res;
+    }
+};
