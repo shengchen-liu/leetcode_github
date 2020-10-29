2ce7816334332919593f712b9a47ef11b2c858d4
lawson1225 <liu.sheng@husky.neu.edu>
2020-10-29 03:33:24 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (cpp)

---
+++ problems/binary_tree_postorder_traversal/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ 
+ helper(node->left)
+ helper(node->right)
+ node
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        stack<TreeNode*> s{{root}};
+        while (!s.empty()) {
+            TreeNode* t = s.top();
+            s.pop();
+            res.insert(res.begin(), t->val);
+            if (t->left) s.push(t->left);
+            if (t->right) s.push(t->right);
+        }
+        return res;
+    }
+    
+
+};
