892c1f35e628deeb842ba15174675c0cf7106567
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-01 03:08:57 +0000

    Sync LeetCode submission - Check Array Formation Through Concatenation (cpp)

---
+++ problems/check_array_formation_through_concatenation/solution.cpp
@@ -0,0 +1,49 @@
+/*
+ [[78],[4,64],[91]]
+vector<int> (101) a
+78: {0}
+4: {64}
+91: {0}
+for i in range(arr.size):
+    find m[i].
+        if false => return false
+        if true :
+            for j = 0 : m[i]
+                check if arr[i] == m[j]
+                if false return false
+                if true: ++i, ++j
+return true
+*/
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        unordered_map<int ,vector<int>> m;
+        for (auto a : pieces) {
+            m[a[0]] = a;
+        }
+        
+        // loop
+        int i = 0;
+        while (i < arr.size()) {
+            int val = arr[i]; //91, 4
+            if (!m.count(val)) {
+                cout << val << endl;
+                return false;
+            } else{
+                auto tmp = m[val]; // {91}, {4, 64}
+                int n = tmp.size(); //1, 2
+                cout << n << endl;
+                int j = 0;
+                while (j < n) {
+                    if (arr[i] != tmp[j]){
+                        // cout << arr[i] << "," << tmp[j];
+                        return false;
+                    }
+                    ++i;
+                    ++j;
+                }
+            }
+        }
+        return true;
+    }
+};
