6be6f766d904af0601b9cfb22f4f838ad6e04efc
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-03 23:21:03 +0000

    Sync LeetCode submission - Rotate List (cpp)

---
+++ problems/rotate_list/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ 
+     1->2->3->4->5->NULL, k = 2
+  1) 5->1->2->3->4->NULL
+  2) 4->5->1->2->3->NULL
+  len = 5
+  k' = rotate k % 5 times
+k' = 0 don't rotate
+k' > 0 rotate
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head) return head;
+        
+        int len=1; // number of nodes
+        ListNode *newH, *tail;
+        newH=tail=head;
+        
+        while(tail->next)  // get the number of nodes in the list
+        {
+            tail = tail->next;
+            len++;
+        }
+        tail->next = head; // circle the link
+
+        if(k %= len) 
+        {
+            for(auto i=0; i<len-k; i++) tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
+        }
+        newH = tail->next; 
+        tail->next = NULL;
+        return newH;
+    }
+};
