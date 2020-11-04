80babbff036fa7e296286cd4c834d5ce663a5838
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-04 17:59:02 +0000

    Sync LeetCode submission - Generate Parentheses (cpp)

--- problems/generate_parentheses/solution.cpp
+++ problems/generate_parentheses/solution.cpp
@@ -1,40 +1,52 @@
 /*
-1. # of ( == # of )
-2. at position i, # of ( >= # of )
-    set a cnt >= 0
-n pairs
-1st position: (
+N = 3
+dfs
+observatation:
+1. idx '0' must be (   
+2.diff = num_right_rem - num_left_rem
+diff must >= 0 to be valid
+                                     left_remain    right_remain
+                       (              2             3 
+                     {2, 3}                     
+                     (      )  
+                   {1,3}   {2,2}
+                   (   )      (     )
+                {0,3} {1,2} {1,2} {2,1}
+                   ) (  )
+                   )
+                   )
+...
+                )
+                {0,0}
 
-dfs (back tracking)
+string dfs(int left, int right, vector<string>& res)
+    if diff >= 0:
+        dfs(left-1, right, diff, res)
+        dfs(left, right-1, diff, res)
 */
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        string out;
-        helper(0, n, 0, 0, res, out);
+        dfs(n, n, res, "");
         return res;
+        
     }
     
-    // at position i, generate current string : out
-    void helper(int i, 
-                int n,
-                int left,
-                int right,
-                vector<string>& res, 
-                string out) {
-        if (i == 2 * n) {
-            res.push_back(out); // i : 0, 1
+    void dfs(int left_rem, int right_rem, vector<string>& res, string path) {
+        // termination
+        if (left_rem == 0 && right_rem == 0){
+            res.push_back(path);
             return;
         }
         
-        // out + '('
-        if (left < n) {
-            helper(i + 1, n, left + 1, right, res, out + '(');
-        }
-        // out + ')'
-        if (left > right) {
-            helper(i + 1, n, left, right + 1, res, out + ')');
-        }
+        int diff = right_rem - left_rem; // >=0
+        if (diff < 0) return;
+        
+        // recursion
+        if (left_rem > 0)
+            dfs(left_rem - 1, right_rem, res, path + "(");
+        if (right_rem > 0)
+            dfs(left_rem, right_rem - 1, res, path + ")");
     }
 };
