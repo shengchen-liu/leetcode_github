f4951a1245ab4ca018f185bf10b417903e1cf140
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-04 05:07:33 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- problems/climbing_stairs/solution.cpp
+++ problems/climbing_stairs/solution.cpp
@@ -18,18 +18,14 @@ dp[3] = dp[1] + dp[2] = 1 + 2 = 3
 class Solution {
 public:
     int climbStairs(int n) {
-        // vector<int> dp(n + 1);
-        // dp[0] = 1;
-        // dp[1] = 1;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
         if (n == 1) return 1;
-        int back2 = 1;
-        int back1 = 1;
-        int cur = 0;
+
         for (int i = 2; i < n + 1; ++i) {
-            cur =  back2 + back1;
-            back2 = back1;
-            back1 = cur;
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return cur;
+        return dp[n];
     }
 };
