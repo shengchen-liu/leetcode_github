4cbd0f3ec659b6207783a9590324c38136bb1f0e
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-04 06:02:03 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (cpp)

--- problems/kth_largest_element_in_an_array/solution.cpp
+++ problems/kth_largest_element_in_an_array/solution.cpp
@@ -1,20 +1,28 @@
-/*
-min heap, size of k
-
-*/
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        auto cmp = [](int &n1, int &n2) {return n1 > n2;};
-        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
-        for (int num : nums) {
-            if (pq.size() < k) pq.push(num);
-            else if (num > pq.top()) {
-                pq.pop();
-                pq.push(num);
-            }
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int pivot = partition(nums, left, right);
+            if (pivot == k - 1) return nums[pivot];
+            if (pivot > k - 1) right = pivot - 1;
+            else left = pivot + 1;
         }
-        return pq.top();
+        return -1;
         
+    }
+    
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) ++l;
+            if (nums[r] <= pivot) --r;
+        }
+        swap(nums[left], nums[r]);
+        return r;
     }
 };
