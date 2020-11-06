33a6645bc782270838459f4699a0ead847ba10a9
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-06 18:50:38 +0000

    Sync LeetCode submission - Insertion Sort List (cpp)

---
+++ problems/insertion_sort_list/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ 4->2->1->3
+ i            4->2->1->3
+    i         2->4->1->3
+    
+
+pre->next = cur->next
+find a
+b = a->next
+a->next = cur
+cur->next= b
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *dummy = new ListNode(-1), *cur = dummy;
+        while (head) {
+            ListNode *t = head->next;
+            cur = dummy;
+            while (cur->next && cur->next->val <= head->val) {
+                cur = cur->next;
+            }
+            head->next = cur->next;
+            cur->next = head;
+            head = t;
+        }
+        return dummy->next;
+    }
+};
