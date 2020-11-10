475f2a516d9467373372862ad33164c72b31304b
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-10 16:53:35 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,21 +1,40 @@
+/*
+s = "ADOBECODEBANC", t = "ABC"
+hashmap:
+A: 1
+B: 1
+C: 1
+ADOBECODEBANC     map           cnt    
+ij                A:0 B:1 C:1   1 
+i j               A:0 B:1 C:1   1
+i  j              A:0 B:0 C:1   2
+i   j             A:0 B:0 C:1   2
+i    j            A:0 B:0 C:0   3    move left, update min_len
+ i   j            A:1 B:0 C:0   2    move     
+ i       j        A:0 B:0 C:0   3    move left
+  i      j        A:0 B:0 C:0
+   i  j 
+   i   j
+   i    j
+   i     j
+   i      j                 B:2, C:1
+   i       j                A:1, B:2, C:1
+*/
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        unordered_map<char, int> m;
-        for (auto a : t) ++m[a];
-        int left = 0;
-        int count = 0;
-        int minLen = INT_MAX;
         string res = "";
-        for (int right = 0; right < n; ++right) {
-            if (--m[s[right]] >= 0) ++count;
-            while (count == t.size() ) {
-                if (minLen > right - left + 1) {
-                    minLen = right - left + 1;
+        unordered_map<char, int> letterCnt;
+        int left = 0, cnt = 0, minLen = INT_MAX;
+        for (char c : t) ++letterCnt[c];
+        for (int i = 0; i < s.size(); ++i) {
+            if (--letterCnt[s[i]] >= 0) ++cnt;
+            while (cnt == t.size()) {
+                if (minLen > i - left + 1) {
+                    minLen = i - left + 1;
                     res = s.substr(left, minLen);
                 }
-                if (++m[s[left]] > 0) --count;
+                if (++letterCnt[s[left]] > 0) --cnt;
                 ++left;
             }
         }
