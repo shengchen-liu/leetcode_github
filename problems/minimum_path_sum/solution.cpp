a3962f409cf8b22072e8315d8227a9888bb6e680
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-17 03:23:46 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

--- problems/minimum_path_sum/solution.cpp
+++ problems/minimum_path_sum/solution.cpp
@@ -1,21 +1,46 @@
 /*
-dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1])
+1 3 1
+1 5 1
+4 2 1
+
+1 3
+1 5
+
+2 path: 1->3->5; 1->1->5
+dp[i][j]: min path sum from (0,0) to (i,j)
+dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+
 */
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (i == 0 && j == 0) dp[i][j] = grid[i][j];
-                else if (i == 0) dp[i][j] = dp[i][j - 1] + grid[i][j];
-                else if (j == 0) dp[i][j] = dp[i - 1][j] + grid[i][j];
-                else dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+                // Boundary condition
+                // (0, 0)
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                }
+                
+                // 1st row
+                else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                }
+                
+                // 1st col
+                else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+                
+                // general relation
+                else {
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 };
