1bd5adb35de913136d0499fa1b7df6dcd33d3107
lawson1225 <liu.sheng@husky.neu.edu>
2020-11-17 18:53:10 +0000

    Sync LeetCode submission - Copy List with Random Pointer (cpp)

--- problems/copy_list_with_random_pointer/solution.cpp
+++ problems/copy_list_with_random_pointer/solution.cpp
@@ -1,27 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+1.creat new nodes
+hashmap : {old : new}
+
+2. for each old node:
+    new_nodes = hashmap[old]
+    new_nodes->next = hashmap[old->next]
+    new_nodes->random = hashmap[old->random]
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if (!head) return nullptr;
-        Node *cur = head;
-        while (cur) {
-            Node *t = new Node(cur->val, nullptr, nullptr);
-            t->next = cur->next;
-            cur->next = t;
-            cur = t->next;
+        
+        unordered_map<Node*, Node*> m;
+        // create new nodes
+        Node* old = head;
+        while (old) {
+            m[old] = new Node(old->val);
+            old = old->next;
         }
-        cur = head;
-        while (cur) {
-            if (cur->random) cur->next->random = cur->random->next;
-            cur = cur->next->next;
+        
+        // for each old node
+        old = head;
+        while (old) {
+            Node* new_node = m[old];
+            new_node->next = m[old->next];
+            new_node->random = m[old->random];
+            old = old->next;
         }
-        cur = head;
-        Node *res = head->next;
-        while (cur) {
-            Node *t = cur->next;
-            cur->next = t->next;
-            if (t->next) t->next = t->next->next;
-            cur = cur->next;
-        }
-        return res;
+        return m[head];
     }
 };
