93c6838ff4df7e3141e2e662924c5ac4379db7a0
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-04 04:44:49 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- problems/sliding_window_maximum/solution.cpp
+++ problems/sliding_window_maximum/solution.cpp
@@ -1,33 +1,3 @@
-/*
-1,3,-1,-3,5,3,6,7, k = 3
-1  3  -1 -3  5  3  6  7    deque(idx)                    max
-i      j                   [1, 2]                    3
-i         j                [1, 2, 3]          3
-   i      j                [2, 3]              3
-   i         j             [5]         5
-       i     j             [5]              5
-       i        j          [5, 3]          5
-          i     j          [5, 3]               5
-          i        j       [6]           ?
-             i     j       [6]                6
-             i        j    [7]            ?
-                i     j    [7]                7
-deque dq
-max
-for i from 0 to k-1:
-    initialize dq
-    while (val > dq.back()){
-        dq.pop_back()
-    }
-    dq.push_back(val)
-    
-for i from k to n - 1:
-    push val to dq
-    if nums[i] == dq.front:
-        dq.pop_front
-    res.push_back(dq.front())
-            
-*/
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
