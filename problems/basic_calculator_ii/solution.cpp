e24cfb8d6b2036921b7d5ceba87803f662bd687d
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-15 04:48:47 +0000

    Sync LeetCode submission - Basic Calculator II (cpp)

--- problems/basic_calculator_ii/solution.cpp
+++ problems/basic_calculator_ii/solution.cpp
@@ -1,43 +1,74 @@
+/*
+3+2*2
+case1: digit
+update num
+
+case2: +
+sign: 1
+stack.push num
+
+case3: -
+sign: -1
+stack.push num
+
+2 * 2 + 1
+case4: * => handle the operator now
+stack push num
+operator: *
+
+case5: /
+stack push num
+operator: /
+
+case 6: space
+if operator is * or /:
+    pop stack.top
+    num = num [operator] pop.top
+    push back num to stack
+
+case 7: end of the string
+    
+
+
+
+
+*/
 class Solution {
 public:
     int calculate(string s) {
         int res = 0;
+        int curNumber = 0;
+        int last = 0;
+        char op = '+';
         int n = s.size();
-        stack<int> st;
-        char oper = '+';
-        int num = 0;
         
+        // iter
         for (int i = 0; i < n; ++i) {
-            // cout << num << endl;
-            char c = s[i];
-            // 1. digit
-            if (c - '0' >= 0 && c - '9' <= 0) {
-                num = num * 10 + (c - '0');
+            char cur = s[i];
+            // case 1: digit
+            if (cur >= '0' && cur <= '9'){
+                curNumber = curNumber * 10 + (cur - '0');
             } 
-            if (c != ' ' && !(c - '0' >= 0 && c - '9' <= 0) || i == n - 1) {
-                if (oper == '+'){
-                    st.push(num);
-                } else if (oper == '-') {
-                    st.push(-num);
-                } else if (oper == '*') {
-                    int tmp = num * st.top();
-                    st.pop();
-                    st.push(tmp);
-                } else if (oper == '/') {
-                    int tmp = st.top() /  num;
-                    st.pop();
-                    st.push(tmp);
+            if ((cur < '0' && cur != ' ') || i == n - 1) {
+                if (op == '+'){
+                    res += last;
+                    last = curNumber;
                 }
-                num = 0;
-                oper = c;
-            } 
+                else if (op == '-'){
+                    res += last;
+                    last = -curNumber;
+                }
+                else if (op == '*' ){
+                    last = last * curNumber;
+                } else if (op = '/'){
+                    last = last / curNumber;
+                }
+                op = cur;
+                curNumber = 0;
+            }
         }
-        
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
-        }
-        
+            
+        res += last;
         return res;
     }
 };
