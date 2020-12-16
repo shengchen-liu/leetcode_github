00d28bc164bf44586e23d72d9e0ff36979ba3a9b
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-16 22:58:05 +0000

    Sync LeetCode submission - Rotting Oranges (cpp)

--- problems/rotting_oranges/solution.cpp
+++ problems/rotting_oranges/solution.cpp
@@ -1,87 +1,49 @@
-/**
-visited? 
-**/
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        //visited table
-        int n_rows = grid.size();  //2
-        int n_cols = grid[0].size(); //2
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
         
-        vector<vector<int>> visited(n_rows, vector<int>(n_cols, false));
+        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
         
-        int minute = 0;
+        int m=grid.size();
+        int n=grid[0].size();
         
-        queue<int> q_x;
-        queue<int> q_y;
-        
-        // note: count all the fresh oranges.
-        int fresh = 0;
-        
-        for (int row = 0; row < n_rows; row++){
-            for (int col = 0; col < n_cols; col++){
-                // find the first rotten [0, 0]
-                if (grid[row][col] == 2 ){
-                    // queue
-                    q_x.push(col);
-                    q_y.push(row);
+        queue<pair<int,int>> q;
+        int fresh=0; //To keep track of all fresh oranges left
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                if(grid[i][j]==1)
+                    fresh++;
+            }
+        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
+        while(!q.empty())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int> p=q.front();
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
+                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
+                    {
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
+                    }
                     
-                    visited[row][col] = true;
-                }
-                if (grid[row][col] == 1) fresh++;
-                
-            }
-        }
-        
-        //if no fresh at all
-        if (fresh == 0 ) return 0;
-        
-        vector<int> x_dir{0, 0, -1, 1};
-        vector<int> y_dir{-1, 1, 0, 0};
-        
-
-        while (!q_x.empty()){
-            ++minute;
-            int size = q_x.size(); // 1
-            for (int count = 0; count < size; count ++){
-                int x = q_x.front();
-                int y = q_y.front();
-                q_x.pop();
-                q_y.pop();
-                // [0,0]
-                // for neighbor ->q: [[[0,1], [1, 0]]
-                for (int i = 0; i < 4; i++){
-                    int x_new = x + x_dir[i];
-                    int y_new = y + y_dir[i];
-
-                    //inbound?
-                    if (!inBound(x_new, y_new, n_rows, n_cols)) continue;
-
-                    // visited?
-                    if (visited[y_new][x_new]) continue;
-                    visited[y_new][x_new] = true;
-
-                    // [0, 1]
-                    if (grid[y_new][x_new] == 0) continue; //empty
-                    q_x.push(x_new);
-                    q_y.push(y_new);
-                    if (grid[y_new][x_new] == 1){ // fresh
-                        grid[y_new][x_new] = 2; // fresh -> bad
-                    } // q[[0, 1]]
                 }
             }
+            ans++; //incremented after each minute passes
         }
+        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
+        return ans;
         
-        minute--;
-        for (int row = 0; row < n_rows; row++){
-            for (int col = 0; col < n_cols; col++){
-                if (grid[row][col] == 1) return -1;
-            }
-        }
-        return minute;
-    }
-    
-    bool inBound(int x_new, int y_new, int n_rows, int n_cols){
-        return x_new >= 0 && x_new < n_cols && y_new >=0 && y_new < n_rows;
     }
 };
