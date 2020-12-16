2e239aa1ce1a83af0f0d8c2fdc73b28f6f61b768
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-16 21:36:06 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- problems/validate_binary_search_tree/solution.cpp
+++ problems/validate_binary_search_tree/solution.cpp
@@ -9,21 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+low < node < high
+for each node:
+    update low, high
+    compare node's val vs low,high
+*/
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        TreeNode *pre = NULL;
-        return helper(root, pre);
+        return dfs(root, LONG_MIN, LONG_MAX);
     }
     
-    bool helper(TreeNode* node, TreeNode* &pre) {
-        if (!node) return true;
-        bool res = helper(node->left, pre);
-        if (!res) return false;
-        if (pre) {
-            if (node->val <= pre->val) return false;
-        }
-        pre = node;
-        return helper(node->right, pre);
+    bool dfs(TreeNode* node, long low, long high) {
+        // termination
+        if (!node)
+            return true;
+        
+        // recursion
+        if (node->val <= low || node->val >= high)
+            return false;
+        bool left_res = dfs(node->left, low, node->val);
+        bool right_res = dfs(node->right, node->val, high);
+        return left_res && right_res;
     }
 };
