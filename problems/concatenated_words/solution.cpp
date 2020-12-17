14636d16e1dce661afef748c9a7e62f62c39ce58
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-17 00:44:54 +0000

    Sync LeetCode submission - Concatenated Words (cpp)

--- problems/concatenated_words/solution.cpp
+++ problems/concatenated_words/solution.cpp
@@ -1,26 +1,45 @@
+/*
+dfs(i):
+    cnt >= 2
+    [0, i) substr in hashset
+    AND
+    dfs(i, n, hashset) 
+*/
 class Solution {
 public:
-        vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        if (words.size() <= 2) return {};
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         vector<string> res;
-        unordered_set<string> dict(words.begin(), words.end());
+        int n = words.size();
+        
+        // hashset
+        unordered_set<string> wordSet(words.begin(), words.end());
+        
+        // dfs
         for (string word : words) {
-            dict.erase(word);
-            int len = word.size();
-            if (len == 0) continue;
-            vector<bool> v(len + 1, false);
-            v[0] = true;
-            for (int i = 0; i < len + 1; ++i) {
-                for (int j = 0; j < i; ++j) {
-                    if (v[j] && dict.count(word.substr(j, i - j))) {
-                        v[i] = true;
-                        break;
-                    }
-                }
-            }
-            if (v.back()) res.push_back(word);
-            dict.insert(word);
+            if (word.size() == 0)
+                continue;
+            dfs(wordSet, word, 0, 0, res);
         }
         return res;
+    }
+    
+    // check whether substr[start, end) is a concatenated word
+    bool dfs(unordered_set<string> &wordSet, string word, int start, int cnt, vector<string> &res) {
+        // termination
+        int n = word.size();
+        if (start >= n && cnt >= 2){
+            res.push_back(word);
+            return true;
+        }
+            
+        // recursion
+        for (int i = 1; i <= n - start; ++i) {
+            // substr
+            string tmp = word.substr(start, i);
+            if (wordSet.count(tmp) && dfs(wordSet, word, start + i, cnt + 1, res)) 
+                return true;
+        }
+        return false;
+        
     }
 };
