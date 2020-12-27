3de3a640fc477787faaef59db0be2a7ca6caddf4
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-27 19:46:39 +0000

    Sync LeetCode submission - Where Will the Ball Fall (cpp)

--- problems/where_will_the_ball_fall/solution.cpp
+++ problems/where_will_the_ball_fall/solution.cpp
@@ -1,50 +1,83 @@
+/*
+2d gird mxn
+box: 
+\ : 1
+/ : -1
+stuck: 
+    1. "v" shape
+    2. hits wall
+
+start from row 0, simulate if the ball can reach to the last row
+
+for grid[i][j], check the final destination starting fomr [i,j]
+
+memorization:
+visited: mxn, initial 0
+
+dfs(int i, int j):
+    // termination
+    i  == m:
+        return j
+    // recurse
+    if (visited[i][j] != 0)
+        return visited[i][j]
+        
+    cur = grid[i][j]  // 1, or -1
+    // determine the next stop
+    // check if the ball is stuck
+    if stuck : 
+        return -1
+    update visited = dfs(i + 1. j + cur)
+    return visited[i][j]
+    
+*/
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<int> res(n);
+        
+        vector<int> res(n, 0);
         vector<vector<int>> visited(m, vector<int>(n, 0));
-        // cout << m << "," << n << endl;
-        for (int i = 0; i < n; ++i) {
-            res[i] = check(0, i, grid, visited);
+        
+        // traverse each col
+        for (int j = 0; j < n; ++j) {
+            res[j] = dfs(0, j, grid, visited);
         }
         
         return res;
     }
     
-    // check current grid i, j's final destination
-    int check(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited) {
+    // dfs
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &visited){
         int m = grid.size();
         int n = grid[0].size();
         // termination
         if (i == m)
             return j;
         
-        if (visited[i][j] != 0)
+        // recursion
+        if (visited[i][j] != 0) {
             return visited[i][j];
+        }
         
-        // current dir
+        // cur dir
         int cur = grid[i][j];
         
-        // cout << i << ", " << j << endl;
         // check stuck?
-        // cur = 1 : right wall or v
-        if (cur == 1 && (j == n - 1 || grid[i][j + 1] == -1)) {
+        // cur = 1  \ /
+        if (cur == 1 && (j == n - 1 || grid[i][j + 1] == -1)){
+            visited[i][j] = -1;
+            return -1;
+        }
+        // cur = -1 \ /
+        if (cur == -1 && (j == 0 || grid[i][j - 1] == 1)){
             visited[i][j] = -1;
             return -1;
         }
         
-        // cur = -1 : left wall or v
-        if (cur == -1 && (j == 0 || grid[i][j - 1] == 1)) {
-            visited[i][j] = -1;
-            return -1;
-        }
-        
-        int next = j + cur;
-        // cout << next << endl;
-        visited[i][j] = check(i + 1, next, grid, visited);
-        // cout << next << endl;
+        int next =  j + cur;
+        visited[i][j] =  dfs(i + 1, next, grid, visited);
         return visited[i][j];
     }
 };
