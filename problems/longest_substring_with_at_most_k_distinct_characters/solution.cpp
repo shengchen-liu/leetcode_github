46ad18717fc7f191cf49fa2951ecda688db481b0
lawson1225 <liu.sheng@husky.neu.edu>
2020-12-31 04:19:05 +0000

    Sync LeetCode submission - Longest Substring with At Most K Distinct Characters (cpp)

--- problems/longest_substring_with_at_most_k_distinct_characters/solution.cpp
+++ problems/longest_substring_with_at_most_k_distinct_characters/solution.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        int res = 0;
-        int left = 0;
-        int n = s.size();
-        unordered_map<char, int> hashmap;
-        for (int i = 0; i < n; ++i) {
-            ++hashmap[s[i]];
-            while (hashmap.size() > k) {
-                --hashmap[s[left]];
-                if (hashmap[s[left]] == 0) hashmap.erase(s[left]);
-                ++left;
-            }
-            res = max(res, i - left + 1);
+        if (s.size() == 0 || k == 0) {
+            return 0;
         }
-        return res;
+        
+        int left = 0, right = 0, cnt = 0;
+        int char_set[256] = {0};
+        int ans = 0;
+        
+        while (right < s.size()) {
+            // 统计right指向的字符
+            // 当字符在窗口内第一次出现时，字符种类数+1，该字符出现次数+1
+            if (char_set[s[right]] == 0) {
+                cnt++;
+            }
+            char_set[s[right]]++;
+            right++;
+            
+            // 向右移动left，保持窗口内只有k种不同的字符
+            while (cnt > k) {
+                char_set[s[left]]--;
+                // 当该字符在本窗口不再出现时，字符种类数-1
+                if (char_set[s[left]] == 0) {
+                    cnt--;
+                }
+                left++;
+            }
+            
+            // 更新答案
+            ans = max(ans, right - left);
+        }
+        return ans;
     }
 };
