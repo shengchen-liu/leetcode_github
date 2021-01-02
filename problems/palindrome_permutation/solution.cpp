37d79f85e944c625e88bde7960f2e38c12335ca7
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-02 03:55:24 +0000

    Sync LeetCode submission - Palindrome Permutation (cpp)

--- problems/palindrome_permutation/solution.cpp
+++ problems/palindrome_permutation/solution.cpp
@@ -1,16 +1,25 @@
+/*
+num of odd digits <= 1
+a: 2
+b: 1
+
+*/
 class Solution {
 public:
     bool canPermutePalindrome(string s) {
-        unordered_map<char, int> hashmap;
+        unordered_map<char, int> m;
         for (char c : s) {
-            ++hashmap[c];
+            m[c]++;
         }
         
-        int count = 0;
-        for (auto it : hashmap) {
-            if (it.second % 2 == 1) ++count;
+        int cnt = 0;
+        for (auto a : m) {
+            if (a.second % 2 == 1)
+                ++cnt;
+            if (cnt > 1)
+                return false;
         }
-        if (count > 1) return false;
+    
         return true;
     }
 };
