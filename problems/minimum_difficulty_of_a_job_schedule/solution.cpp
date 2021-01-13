31bbadbc1d85ddfeb93eb02b540a43688ca8cdbb
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-13 06:54:02 +0000

    Sync LeetCode submission - Minimum Difficulty of a Job Schedule (cpp)

--- problems/minimum_difficulty_of_a_job_schedule/solution.cpp
+++ problems/minimum_difficulty_of_a_job_schedule/solution.cpp
@@ -1,26 +1,3 @@
-/*
-6,5,4,3,2,1, d=2
-
-cut the array into d non-empty sub arrays
-d1 = [], d2 = []
-6 _ 5 _ 4 _ 3 _ 2 _ 1 insert d-1 seperators.  try all possible cuts
-6 | 5 _ 4 _ 3 _ 2 _ 1
-6 _ 5 | 4 _ 3 _ 2 _ 1
-6 _ 5 _ 4 | 3 _ 2 _ 1
-6 _ 5 _ 4 _ 3 | 2 _ 1
-6 _ 5 _ 4 _ 3 _ 2 | 1
-
-dp[i][j]: i: min difficulty to schedlue the first i jobs (1-based) in k days
-
-init:
-    dp[0][0] = 0, others: INT_MAX
-transition:
-    dp[i][k] = min{
-        dp[j][k-1] + max(jobs[j+1 ~ i])
-    } for k -1 <= j < i
-
-*/
-
 class Solution {
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
