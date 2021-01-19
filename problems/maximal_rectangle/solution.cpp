e797a0a025d6e6b1e72eeb89584b197416ee77c2
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-19 05:44:46 +0000

    Sync LeetCode submission - Maximal Rectangle (cpp)

--- problems/maximal_rectangle/solution.cpp
+++ problems/maximal_rectangle/solution.cpp
@@ -1,29 +1,56 @@
+/*
+on each row, count how many # of consecutive 1's on the top.
+now this becomes to "find the largest rectangle in a histogram" problem
+*/
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char> > &matrix) {
-        int res = 0;
-        vector<int> height;
-        for (int i = 0; i < matrix.size(); ++i) {
-            height.resize(matrix[i].size());
-            for (int j = 0; j < matrix[i].size(); ++j) {
-                height[j] = matrix[i][j] == '0' ? 0 : (1 + height[j]);
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.size() == 0)
+            return 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> countOnes(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0) {
+                    if (matrix[i][j] == '1')
+                        countOnes[i][j] = 1;
+                } else {
+                    if (matrix[i][j] == '1')
+                        countOnes[i][j] = countOnes[i - 1][j] + 1;
+                }
             }
-            res = max(res, largestRectangleArea(height));
+        }
+        
+        // search each row
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            res = max(res, maxRec(countOnes, i));
         }
         return res;
     }
-    int largestRectangleArea(vector<int>& height) {
-        int res = 0;
-        stack<int> s;
-        height.push_back(0);
-        for (int i = 0; i < height.size(); ++i) {
-            if (s.empty() || height[s.top()] <= height[i]) s.push(i);
-            else {
-                int tmp = s.top(); s.pop();
-                res = max(res, height[tmp] * (s.empty() ? i : (i - s.top() - 1)));
-                --i;
+    
+    int maxRec(vector<vector<int>> &countOnes, int bottom) {
+        stack<int> st;
+        st.push(-1);
+        int maxArea = 0;
+        int n = countOnes[0].size();
+        
+        for (int i = 0; i < n;  ++i) {
+            while (st.top() != -1 && countOnes[bottom][i] <= countOnes[bottom][st.top()]) {
+                int top = st.top();
+                st.pop();
+                maxArea = max(maxArea, countOnes[bottom][top] * (i - st.top() - 1));
             }
+            st.push(i);
         }
-        return res;
+        
+        while (st.top() != -1) {
+            int top = st.top();
+            st.pop();
+            maxArea = max(maxArea, countOnes[bottom][top] * (n - st.top() - 1));
+        }
+        return maxArea;
     }
 };
