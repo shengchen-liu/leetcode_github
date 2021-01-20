98b20646a60f8b38a0e8404112d1530439070865
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-20 03:56:43 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array II (cpp)

--- problems/remove_duplicates_from_sorted_array_ii/solution.cpp
+++ problems/remove_duplicates_from_sorted_array_ii/solution.cpp
@@ -1,32 +1,25 @@
 /*
-0  1  2  3  4  5
-1, 1, 1, 2, 2, 3     
-ij
-i  j
-i     j              erase
+1, 1, 1, 2, 2, 3
+1: 3
+2: 2
+3: 1
+sorted in ascending order
+i: the last good num
+j: fast pointer
+cnt
 
-1, 1, 2, 2, 3    
-      ij
-      
+
+
 */
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-        int i = 0, j = 0;
-        while (i < nums.size()) {
-            while (j < nums.size() && nums[i] == nums[j] && j - i <= 1){
-                ++j;
-            }
-            if (j == nums.size())
-                break;
-            else if (nums[i] != nums[j]){
-                i = j;
-            } else {
-                // erase
-                nums.erase(nums.begin() + j);
+        int i = 0;
+        for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
             }
         }
-        return (int)nums.size();
+        return i;
     }
 };
