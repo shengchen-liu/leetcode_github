6aefda6bc9f65a0cb9f51d22599ae3eff68a8c14
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-21 23:59:54 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- problems/longest_increasing_subsequence/solution.cpp
+++ problems/longest_increasing_subsequence/solution.cpp
@@ -1,16 +1,28 @@
+/*
+O(nlogn)
+dp[i]: the smallest ending number of a subsequence that has length+1
+10,9,2,5,3,7,101,18
+num      dp
+10       [10]  // [10]
+9        [9]   //  [9]
+2        [2]   // [2]
+5        [2,5]  // [2], [*, 5]
+3        [2,3]  // [2], [*, 3]
+7        [2,3,7] // [2], [*,3], [*, *, 7]
+101      [2,3,7,101] // [2], [*,3], [*,*,7], [*,*,*,101]
+18       [2,3,7,18]   //[2], [*,3], [*,*,7], [*,*,*,18]
+*/
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> dp;
-        for (int i = 0; i < nums.size(); ++i) {
-            int left = 0, right = dp.size();
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (dp[mid] < nums[i]) left = mid + 1;
-                else right = mid;
+        for (int x : nums) {
+            auto it = lower_bound(begin(dp), end(dp), x); // find the 1st elem that is >= x
+            if (it == end(dp)){
+                dp.push_back(x);
+            } else {
+                *it = x;
             }
-            if (right >= dp.size()) dp.push_back(nums[i]);
-            else dp[right] = nums[i];
         }
         return dp.size();
     }
