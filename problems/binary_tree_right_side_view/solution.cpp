9367500c239f0c3f62523746236ea0c80e717e4f
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-22 00:41:35 +0000

    Sync LeetCode submission - Binary Tree Right Side View (cpp)

--- problems/binary_tree_right_side_view/solution.cpp
+++ problems/binary_tree_right_side_view/solution.cpp
@@ -9,21 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+BFS
+queue
+  node
+  /   \
+ l     r
+node has right and node has left: queue push right, left
+node has no right, has left : queue push left
+node has no right, no left: nothing
+ 
+layer      queue      res
+0           [1]       []
+1           [3,2]     [1]
+2           [4,5]     [1, 3]
+3           []        [1,3,4]
+
+DFS
+node 
+always visit its right child first
+
+starting from this current node, 
+   node
+  /     \
+ d_l     d_r
+ 
+if it's the first time visit current level, append the node to the res
+
+
+*/
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if (!root) return {};
         vector<int> res;
-        helper(root, res, 0);
+        helper(root, 0, res);
         return res;
     }
     
-    void helper(TreeNode *node, vector<int> &res, int level) {
-        if (!node) return;
-        if (level + 1 > res.size()) { // new level
-            res.push_back(node->val); // res: [1] size = 1
+    void helper(TreeNode* node, int level, vector<int>& res) {
+        // termination
+        if (!node)
+            return;
+        
+        // recursion
+        if (level == res.size()){
+            res.push_back(node->val);
         }
-        if (node->right) helper(node->right, res, level + 1);
-        if (node->left) helper(node->left, res, level + 1);
+        helper(node->right, level + 1, res);
+        helper(node->left, level + 1, res);
     }
 };
