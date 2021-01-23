8ab26c3863d6806e37441acc9893fbda59c313e0
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-23 05:04:22 +0000

    Sync LeetCode submission - Can Place Flowers (cpp)

--- problems/can_place_flowers/solution.cpp
+++ problems/can_place_flowers/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
-        for (int i = 0; i < flowerbed.size(); ++i) {
-            if (flowerbed[i] ==0 &&
-               (i == 0 || flowerbed[i - 1] == 0) &&
-               (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
-                flowerbed[i] = 1;
-                ++count;
+        int i = 0;
+        int cnt = 0;
+        while (i < flowerbed.size()) {
+            if (flowerbed[i] == 0 && (i ==0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)){
+                flowerbed[i++] = 1;
+                ++cnt;
             }
+            if (cnt >= n)
+                return true;
+            ++i;
         }
-        return count >= n;
+        return false;
     }
 };
