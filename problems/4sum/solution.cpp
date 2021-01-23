2586cffd489cc5ebc034c29316749b03113f09d7
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-23 21:59:14 +0000

    Sync LeetCode submission - 4Sum (cpp)

--- problems/4sum/solution.cpp
+++ problems/4sum/solution.cpp
@@ -1,35 +1,45 @@
+/*
+1, 0, -1, 0, -2, 2
+
+*/
 class Solution {
 public:
-vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    sort(begin(nums), end(nums));
-    return kSum(nums, target, 0, 4);
-}
-vector<vector<int>> kSum(vector<int>& nums, int target, int start, int k) {
-    vector<vector<int>> res;
-    if (start == nums.size() || nums[start] * k > target || target > nums.back() * k)
-        return res;
-    if (k == 2)
-        return twoSum(nums, target, start);
-    for (int i = start; i < nums.size(); ++i)
-        if (i == start || nums[i - 1] != nums[i])
-            for (auto &set : kSum(nums, target - nums[i], i + 1, k - 1)) {
-                res.push_back({nums[i]});
-                res.back().insert(end(res.back()), begin(set), end(set));
-            }
-    return res;
-}
-vector<vector<int>> twoSum(vector<int>& nums, int target, int start) {
-    vector<vector<int>> res;
-    int lo = start, hi = nums.size() - 1;
-    while (lo < hi) {
-        int sum = nums[lo] + nums[hi];
-        if (sum < target || (lo > start && nums[lo] == nums[lo - 1]))
-            ++lo;
-        else if (sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1]))
-            --hi;
-        else
-            res.push_back({ nums[lo++], nums[hi--]});
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+       // sort the array
+        sort(nums.begin(), nums.end());
+        return kSum(nums, target, 0, 4);
     }
-    return res;
-}
+    
+    vector<vector<int>> kSum(vector<int>& nums, int target, int start, int k) {
+        vector<vector<int>> res;
+        if (start == nums.size() || nums[start] * k > target || target > nums.back() * k) {
+            return res;
+        }
+        if (k == 2) 
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.size();++i) {
+            if (i == start || nums[i - 1] != nums[i]) {
+                auto a = kSum(nums, target - nums[i], i + 1, k - 1);
+                for (auto row : a) {
+                    res.push_back({nums[i]});
+                    res.back().insert(end(res.back()), begin(row), end(row));
+                }
+            }
+        }
+        return res;
+    }
+    vector<vector<int>> twoSum(vector<int>& nums, int target, int start) {
+        vector<vector<int>> res;
+        int lo = start, hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[lo] + nums[hi];
+            if (sum < target || (lo > start && nums[lo] == nums[lo - 1]))
+                ++lo;
+            else if (sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1]))
+                --hi;
+            else
+                res.push_back({ nums[lo++], nums[hi--]});
+        }
+        return res;
+    }
 };
