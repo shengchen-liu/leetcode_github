9f70f1358b9ba91a0e60912f84fa4cfe7c9f8d2e
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-25 07:20:25 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- problems/sort_colors/solution.cpp
+++ problems/sort_colors/solution.cpp
@@ -1,13 +1,28 @@
 /*
-000, 111, 2222
-2,0,2,1,1,0
-ptrs: i, j, cur
-i: nums to the left are 0s
-j: nums to the right are 2s
-cur: 
-1. nums[cur] == 0, swap nums[i++], nums[cur]
-2. nums[cur] == 2, swap nums[j--], nums[cur]
-3. nums[cur] == 1, ++cur
+2, 0, 2, 1, 1, 0
+ik             j
+0, 0, 2, 1, 1, 2
+ik          j
+0, 0, 2, 1, 1, 2
+   ik       j
+0, 0, 2, 1, 1, 2
+      ik    j
+0, 0, 1, 1, 2, 2
+      ik j
+         k
+   
+all values before i == 0
+all values after j == 2
+
+nums[k] == 2:
+    swap (nums[j], nums[k])
+    --j
+nums[k] == 0:
+    swap (nums[i], nums[k])
+    ++i，++k
+nums[k] == 1:
+    ++k
+
 */
 class Solution {
 public:
@@ -15,15 +30,14 @@ public:
         int n = nums.size();
         int i = 0;
         int j = n - 1;
-        int cur = 0;
-        while (cur <= j) {
-            // 1. nums[cur] == 0, swap nums[i++], nums[cur]
-            if (nums[cur] == 0){
-                swap(nums[i++], nums[cur++]);
-            } else if (nums[cur] == 2){
-                swap(nums[j--], nums[cur]);
+        int k = 0;
+        while (k <= j) {
+            if (nums[k] == 0) {
+                swap(nums[i++], nums[k++]);
+            } else if (nums[k] == 2){
+                swap(nums[j--], nums[k]);
             }
-            else cur++;
+            else k++;
         }
     }
 };
