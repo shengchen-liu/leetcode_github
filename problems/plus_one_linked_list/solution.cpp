d5dfa70fa763f3dd3f88dbdf7d72e1c181ccbc44
lawson1225 <liu.sheng@husky.neu.edu>
2021-01-26 04:21:44 +0000

    Sync LeetCode submission - Plus One Linked List (cpp)

--- problems/plus_one_linked_list/solution.cpp
+++ problems/plus_one_linked_list/solution.cpp
@@ -8,29 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*
+1, 2, 9
+1  3  0
+
+1, 2, 9, 9
+1  3  0  0
+
+from right to left:
+    if num == 9:
+        num = 0
+    else: the 1st num that is not 9 => +1
+        break
+
+stack:
+bot   top
+1, 2, 3
+
+all nums are 9: add 1 to the left
+if head == 0:
+    add a new node(1)
+*/
 class Solution {
 public:
     ListNode* plusOne(ListNode* head) {
+        // stack
+        stack<ListNode*> st;
         ListNode* cur = head;
-        ListNode* right = NULL;
-        // find the last node
-        while (cur){
-            if (cur->val != 9)  // 1 -> 9 -> 1(right) -> 9(cur) -> 9
-                right = cur; // find the right most node that is not 9
+        while (cur) {
+            st.push(cur);
             cur = cur->next;
         }
-        // if all nodes are 9, create a new head
-        if (!right) {
-            right = new ListNode(0);
-            right->next = head;
-            head = right;
+        // st: {3, 2, 1}
+        while (!st.empty()) {
+            cur = st.top();  // 3
+            st.pop(); 
+            if (cur->val == 9)
+                cur->val = 0;
+            else {
+                cur->val++;
+                break;
+            }
         }
         
-        ++right->val;
-        cur = right->next;
-        while (cur){
-            cur->val = 0;
-            cur = cur->next;
+        if (head->val == 0) {
+            // new node
+            ListNode * new_head = new ListNode(1);
+            new_head->next = head;
+            return new_head;
         }
         return head;
     }
