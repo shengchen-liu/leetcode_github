e73e220eafbc1abdb98fb702e8d450c54ee45e51
lawson1225 <liu.sheng@husky.neu.edu>
2021-02-03 05:19:40 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- problems/first_bad_version/solution.cpp
+++ problems/first_bad_version/solution.cpp
@@ -1,22 +1,19 @@
-/*
-binary search
-start, end
-
-*/
-// Forward declaration of isBadVersion API.
-bool isBadVersion(int version);
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int i = 1;
-        int j = n;
-        while (i < j) {
-            int mid = i + (j - i) / 2;
-            if (isBadVersion(mid)) j = mid;
-            else i = mid + 1; // good version
+        int left = 1;
+        int right = n;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)){
+                right = mid;
+            } else {
+                left = mid;
+            }
         }
-        if (isBadVersion(i)) return i;
-        else return j;
+        return isBadVersion(left)? left : right;
     }
 };
