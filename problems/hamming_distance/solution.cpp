f48974384c246d407563b8ba15b0032824fff4ac
lawson1225 <liu.sheng@husky.neu.edu>
2021-02-08 00:05:45 +0000

    Sync LeetCode submission - Hamming Distance (cpp)

--- problems/hamming_distance/solution.cpp
+++ problems/hamming_distance/solution.cpp
@@ -1,11 +1,22 @@
+/*
+0 0 0 1
+0 1 0 0 = 
+0 1 0 1
+
+x ^ y = z
+cnt # of 1s in z
+*/
 class Solution {
 public:
     int hammingDistance(int x, int y) {
         int res = 0;
-        int exec = x ^ y; //XOR
-        while (exec) {
-            ++res;
-            exec &= (exec - 1);
+        int z = x ^ y;
+        // cnt
+        while (z > 0) {
+            int digit = z & 1;
+            if (digit == 1)
+                ++res;
+            z = z >> 1;
         }
         return res;
     }
