42f7c7920e2a0c5999b0a9f3d853a7c9f1b6b037
lawson1225 <liu.sheng@husky.neu.edu>
2021-02-09 23:16:24 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,3 +1,15 @@
+/* 
+1,2,3,4 
+i
+1: f{2,3,4}:
+    1, 2, f{3,4}
+    1, 3, f{2,4}
+2, f{1, 3, 4}
+
+3， f{1,2,4}
+
+4, f{1,2,3}
+*/
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -22,9 +34,9 @@ public:
         // recursion
         // candidates: layer ~ n -1
         for (int i = layer; i < n; ++i) {
-            path.push_back(nums[i]);
-            swap(nums[i], nums[layer]);
-            helper(nums, path, res, layer + 1);
+            path.push_back(nums[i]); // path: 1
+            swap(nums[i], nums[layer]); // i has been visited
+            helper(nums, path, res, layer + 1); // dfs
             path.pop_back();
             swap(nums[i], nums[layer]);
         }
