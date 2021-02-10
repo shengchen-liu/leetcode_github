caf5876760efc11e010c32f0215af4024bc01ab2
lawson1225 <liu.sheng@husky.neu.edu>
2021-02-10 05:00:50 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,18 +1,52 @@
+/*
+0  1  2  3  4  5  6
+2, 5, 6, 0, 0, 1, 2      l     r    m
+l        m        r      0     6    3
+m = 0 + (6 - 0) / 2= 3
+   *m
+ *
+*   
+       *
+     *
+     
+  *   
+* 
+        *
+      *
+    *m
+*/
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size(), left = 0, right = n - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) return true;
-            if (nums[mid] < nums[right]) {
-                if (nums[mid] < target && nums[right] >= target) left = mid + 1;
-                else right = mid - 1;
-            } else if (nums[mid] > nums[right]){
-                if (nums[left] <= target && nums[mid] > target) right = mid - 1;
-                else left = mid + 1;
-            } else --right;
+        if (nums.size() == 0)
+            return false;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            if (nums[left] == nums[right])
+                ++left;
+            else 
+                break;
         }
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] == target || nums[right] == target || nums[mid] == target)
+                return true;
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] < target && target < nums[mid])
+                    right = mid;
+                else 
+                    left = mid;
+            } else {
+                if (nums[mid] < target && target < nums[right])
+                    left = mid;
+                else 
+                    right = mid;
+            } 
+        }
+                    
+        if (nums[left] == target || nums[right] == target)
+            return true;
         return false;
     }
 };
