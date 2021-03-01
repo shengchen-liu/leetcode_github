dd59b0760e2cf1c7888fbb34025c93859c8e1ba3
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-01 05:10:58 +0000

    Sync LeetCode submission - Target Sum (cpp)

--- problems/target_sum/solution.cpp
+++ problems/target_sum/solution.cpp
@@ -1,14 +1,28 @@
+/*
+1, 1, 1, 1, 1
+S: 3
+dp[i][j]: num of ways to assign first i integers, sum up to j
+
+*/
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
-        vector<unordered_map<int, int>> memo(nums.size());
-        return helper(nums, S, 0, memo);
-    }
-    int helper(vector<int>& nums, long sum, int start, vector<unordered_map<int, int>>& memo) {
-        if (start == nums.size()) return sum == 0;
-        if (memo[start].count(sum)) return memo[start][sum];
-        int cnt1 = helper(nums, sum - nums[start], start + 1, memo);
-        int cnt2 = helper(nums, sum + nums[start], start + 1, memo);
-        return memo[start][sum] = cnt1 + cnt2;
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        if (S > sum || sum < -S)
+            return 0;
+        if ((S + sum) & 1) 
+            return 0;
+        int target = (S + sum) / 2;
+        
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= nums.size(); ++i) {
+            for (int j = target; j >= nums[i-1]; --j) {
+                dp[j] = dp[j] + dp[j - nums[i - 1]];
+            }
+        }
+        return dp[target];
     }
 };
