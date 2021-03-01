e82ecbea12edde19b5e49f4a516ab4611a020a4b
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-01 23:29:09 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (cpp)

--- problems/longest_palindromic_subsequence/solution.cpp
+++ problems/longest_palindromic_subsequence/solution.cpp
@@ -1,28 +1,18 @@
-/*
-dp[i][j]: longest palin sub from idx i to idx j.
-
-bbb => dp[2] = 3
-bbb a => 
-1. s[i] == s[j]: dp[i][j] = dp[i + 1][j - 1] + 2
-2. s[i] != s[j] : dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
-    the max sub pal does not include 
-*/
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        int n = s.size(), res = 0;
-        vector<int> dp(n, 1);
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         for (int i = n - 1; i >= 0; --i) {
-            int len = 0;
+            dp[i][i] = 1;
             for (int j = i + 1; j < n; ++j) {
-                int t = dp[j];
                 if (s[i] == s[j]) {
-                    dp[j] = len + 2;
-                } 
-                len = max(len, t);
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
             }
         }
-        for (int num : dp) res = max(res, num);
-        return res;
+        return dp[0][n - 1];
     }
 };
