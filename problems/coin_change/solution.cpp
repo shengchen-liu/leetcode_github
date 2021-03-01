372a0b4372739ed8d22fe573b63955491932820f
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-01 04:48:43 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- problems/coin_change/solution.cpp
+++ problems/coin_change/solution.cpp
@@ -1,16 +1,26 @@
+/*
+coins: 1, 2, 5
+amount: 11
+11 = 5 + 5 + 1
+fewest number of coins
+dp[i]: fewest num of coins to sum up to i
+for each coin in coins:
+    dp[i] = min(dp[i - coin] + 1, dp[i])
+
+*/
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // dp[i]: fewest number of coins for amount i
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; ++i) {
-            for (int j = 0; j < coins.size(); ++j) {
-                if (coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }   
+            } 
         }
-        return (dp[amount] > amount) ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
+
     }
 };
