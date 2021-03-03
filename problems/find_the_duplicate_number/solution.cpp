66bd384ff8ff846492e4f4f3291a34326c00a4b2
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-03 04:54:59 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

--- problems/find_the_duplicate_number/solution.cpp
+++ problems/find_the_duplicate_number/solution.cpp
@@ -1,28 +1,3 @@
-/*
-[1 , n]  n + 1
-
-1, 3, 4, 2, 2
-
-idea 1: sort nums (O(n log(n)))
-1, 2, 2, 3, 4 o(n) , o (log(n))
-
-idea 2: counting
-bucket size: n + 1
-idx: 0, 1, 2, ..., n n = 5
-   [0, 0, 0, 0, 0, 0]
-1: [0, 1, 0, 0, 0, 0]
-3: [0, 1, 0, 1, 0, 0]
-4: [0, 1, 0, 1, 1, 0]
-2: [0, 1, 1, 1, 1, 0]
-2: [0, 1, 1[x], 1, 1, 0]
-
-idea 3: 
-0 -> 1
-1 -> 3
-2 -> 4
-3 -> 2
-4 -> 2
-*/
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
