1a5ef323c7f2e1f0104fea4485cf15dd7e631719
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-11 02:06:59 +0000

    Sync LeetCode submission - Design HashMap (cpp)

--- problems/design_hashmap/solution.cpp
+++ problems/design_hashmap/solution.cpp
@@ -1,40 +1,44 @@
 class MyHashMap {
+    vector<list<pair<int,int>>> m_data;
+    size_t m_size = 10000;
 public:
     /** Initialize your data structure here. */
     MyHashMap() {
-        data.resize(1000, vector<int>());
+        m_data.resize(m_size);
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) {
-        int hashKey = key % 1000;
-        if (data[hashKey].empty()) {
-            data[hashKey].resize(1000, -1);
+        auto &list = m_data[key % m_size];
+        for (auto & val : list) {
+            if (val.first == key) {
+                val.second = value;
+                return;
+            }
         }
-        data[hashKey][key / 1000] = value;
+        list.emplace_back(key, value);
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        int hashKey = key % 1000;
-        if (!data[hashKey].empty()) {
-            return data[hashKey][key / 1000];
+        const auto &list = m_data[key % m_size];
+        if (list.empty()) {
+            return -1;
+        }
+        for (const auto & val : list) {
+            if (val.first == key) {
+                return val.second;
+            }
         }
         return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        int hashKey = key % 1000;
-        if (!data[hashKey].empty()) {
-            data[hashKey][key / 1000] = -1;
-        }        
+        auto &list = m_data[key % m_size];
+        list.remove_if([key](auto n) { return n.first == key; });
     }
-
-private:
-    vector<vector<int>> data;
 };
-
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap* obj = new MyHashMap();
