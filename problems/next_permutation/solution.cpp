77e3fc7b19ebd4159cfd16123b90e1275824bd9f
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-18 04:19:05 +0000

    Sync LeetCode submission - Next Permutation (cpp)

--- problems/next_permutation/solution.cpp
+++ problems/next_permutation/solution.cpp
@@ -1,32 +1,26 @@
 /*
-6 2 1 5 4 3 0
-    i
-    
-6 2 3 5 4 1 0
-    i
-reverse 
-6 2 3 0 1 4 5
+1,2,3,4 -> 1,2,4,3
+1,3,2,4 -> 1,3,4,2
+1,4,2,3 -> 1,4,3,2
+1,4,3,2 -> 2,1,3,4
 
-from right to left, check if the sub combinatinon is decreasing order
 */
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2; 
-        while (i >= 0 && nums[i + 1] <= nums[i])
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
             --i;
         
-        // now i is the num to swap
-        // find the num to swap with, which is the next largest number than nums[i]
-        if (i >= 0){
+        // now is the number to swap
+        // find the num to swap with, nums[j] is the next largest number than nums[i]
+        if (i >= 0) {
             int j = n - 1;
             while (j >= 0 && nums[j] <= nums[i])
                 --j;
             swap(nums[i], nums[j]);
         }
-        
-        // now from i + 1 to n-1 is a decreasing order.  we need to reverse it 
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
