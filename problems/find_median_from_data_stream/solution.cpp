707fe0eafa8ba004afb6c6818e8e16e7c3f27ee2
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-29 06:20:29 +0000

    Sync LeetCode submission - Find Median from Data Stream (cpp)

--- problems/find_median_from_data_stream/solution.cpp
+++ problems/find_median_from_data_stream/solution.cpp
@@ -1,52 +1,28 @@
 class MedianFinder {
-    multiset<int> data;
-    multiset<int>::iterator lo_median, hi_median;
+    priority_queue<int> lo;                              // max heap
+    priority_queue<int, vector<int>, greater<int>> hi;   // min heap
 
 public:
-    MedianFinder()
-        : lo_median(data.end())
-        , hi_median(data.end())
-    {
-    }
-
+    // Adds a number into the data structure.
     void addNum(int num)
     {
-        const size_t n = data.size();   // store previous size
+        lo.push(num);                                    // Add to max heap
 
-        data.insert(num);               // insert into multiset
+        hi.push(lo.top());                               // balancing step
+        lo.pop();
 
-        if (!n) {
-            // no elements before, one element now
-            lo_median = hi_median = data.begin();
-        }
-        else if (n & 1) {
-            // odd size before (i.e. lo == hi), even size now (i.e. hi = lo + 1)
-
-            if (num < *lo_median)       // num < lo
-                lo_median--;
-            else                        // num >= hi
-                hi_median++;            // insertion at end of equal range
-        }
-        else {
-            // even size before (i.e. hi = lo + 1), odd size now (i.e. lo == hi)
-
-            if (num > *lo_median && num < *hi_median) {
-                lo_median++;                    // num in between lo and hi
-                hi_median--;
-            }
-            else if (num >= *hi_median)         // num inserted after hi
-                lo_median++;
-            else                                // num <= lo < hi
-                lo_median = --hi_median;        // insertion at end of equal range spoils lo
+        if (lo.size() < hi.size()) {                     // maintain size property
+            lo.push(hi.top());
+            hi.pop();
         }
     }
 
+    // Returns the median of current data stream
     double findMedian()
     {
-        return ((double) *lo_median + *hi_median) * 0.5;
+        return lo.size() > hi.size() ? lo.top() : ((double) lo.top() + hi.top()) * 0.5;
     }
 };
-
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder* obj = new MedianFinder();
