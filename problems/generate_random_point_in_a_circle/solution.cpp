91677d0de3185b6463fec4f66bd57d0b0a78137a
lawson1225 <liu.sheng@husky.neu.edu>
2021-03-29 06:38:27 +0000

    Sync LeetCode submission - Generate Random Point in a Circle (cpp)

--- problems/generate_random_point_in_a_circle/solution.cpp
+++ problems/generate_random_point_in_a_circle/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+    double rad, xc, yc;
+    //c++11 random floating point number generation
+    mt19937 rng{random_device{}()};
+    uniform_real_distribution<double> uni{0, 1};
+
     Solution(double radius, double x_center, double y_center) {
-        r = radius;
-        centerX = x_center;
-        centerY = y_center;
+        rad = radius, xc = x_center, yc = y_center;
     }
-    
+
     vector<double> randPoint() {
-        double theta = 2 * M_PI * ((double) rand() / RAND_MAX);
-        double len = sqrt((double) rand() / RAND_MAX) * r;
-        return {centerX + len * cos(theta), centerY + len * sin(theta)};
-        
+        double d = rad * sqrt(uni(rng));
+        double theta = uni(rng) * (2 * M_PI);
+        return {d * cos(theta) + xc, d * sin(theta) + yc};
     }
-private:
-    double r;
-    double centerX;
-    double centerY;
 };
 
 /**
