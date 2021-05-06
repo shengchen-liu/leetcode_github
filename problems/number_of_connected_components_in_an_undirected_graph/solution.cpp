66802ff2d4908590d69d0fb211f02cf62658d2a7
lawson1225 <liu.sheng@husky.neu.edu>
2021-05-06 05:00:44 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (cpp)

--- problems/number_of_connected_components_in_an_undirected_graph/solution.cpp
+++ problems/number_of_connected_components_in_an_undirected_graph/solution.cpp
@@ -3,21 +3,24 @@ public:
     int countComponents(int n, vector<vector<int>>& edges) {
         int res = n;
         vector<int> father(n);
-        for (int i = 0; i < n; ++i) father[i] = i;
-        for (auto a : edges) {
-            int x = find(a[0], father);
-            int y = find(a[1], father);
+        for (int i = 0; i < n; ++i)
+            father[i] = i;
+        
+        for (auto edge : edges) {
+            int x = find(edge[0], father);
+            int y = find(edge[1], father);
             if (x != y) {
                 father[y] = x;
                 --res;
             }
         }
+        
         return res;
-    } 
+    }
     
-private:
     int find(int x, vector<int> &father) {
-        while (x != father[x]) x = father[x];
+        while (x != father[x])
+            x = father[x];
         return x;
     }
 };
