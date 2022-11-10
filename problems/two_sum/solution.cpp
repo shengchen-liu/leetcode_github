8b3821fd66547554f73a5eb158bdf809a4905e6e
lawson1225 <liu.sheng@husky.neu.edu>
2022-11-10 01:51:25 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,17 +1,48 @@
+/*
+nums = [2, 7, 11, 15]
+target = 9
+
+return: 
+[0, 1]
+
+a + b = target
+traverse each a idx in nums:
+    for each b idx in nums:
+        if a + b == target:
+            return [a, b]
+
+O(n^2)
+
+solution 2:
+hashmap:
+2: 0
+7: 1
+11: 2
+15: 3
+
+for each val in hashmap:
+    if map(target - val) exists
+        return (val, map(target - val))
+
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
+        unordered_map<int, int> hashmap;
+        vector<int> res;
+
+        // construct hashmap
         for (int i = 0; i < nums.size(); ++i) {
-            m[nums[i]] = i;
+            hashmap[nums[i]] = i;
         }
-        
+        // traverse each val
         for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            if (m.count(target - num) && i != m[target - num]) {
-                return {i, m[target - num]};
+            if (hashmap[target - nums[i]] && i != hashmap[target - nums[i]]) {
+                res = {i, hashmap[target - nums[i]]};
+                break;
             }
         }
-        return {};
+
+        return res;
     }
 };
