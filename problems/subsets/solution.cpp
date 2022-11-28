7712b5c9ab6330d58016b41a7aaf019bb2ebaebd
lawson1225 <liu.sheng@husky.neu.edu>
2022-11-28 06:24:13 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,28 +1,31 @@
+/*
+
+1 2 3
+0 0 0
+0 0 1
+0 1 0
+1 0 0
+
+
+*/
 class Solution {
 public:
-    void dfs(vector<int>& nums, vector<vector<int>> &res, vector<int> &tmp, int i) {
-        // termination
-        int n = nums.size();
-        if (i == n){
-            res.push_back(tmp);
-            return;
-        }
-        
-        // recursion
-        // don't include current val
-        dfs(nums, res, tmp, i + 1);
-        
-        // include current val
-        tmp.push_back(nums[i]);
-        dfs(nums, res, tmp, i + 1);
-        tmp.pop_back();
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        // back tracking
-        vector<int> tmp;
-        dfs(nums, res, tmp, 0);
+        int n = nums.size();
+        int p = 1 << n;  // '1000'
+        vector<vector<int>> res;  // size = 8
+        for (int i = 0; i < p; ++i) {  // bit mask   '100'
+            // subset corresponding to that bitmask
+            vector<int> tmp;
+            for (int j = 0; j < n; ++j) {  
+                if ((i >> j) & 1) {
+                    tmp.push_back(nums[j]);
+                }
+            }
+            res.push_back(tmp);
+        }
+
         return res;
+        
     }
 };
