9096effbad6a46967a2bc01b8a993c6daf165bba
lawson1225 <liu.sheng@husky.neu.edu>
2022-11-29 00:25:11 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,44 +1,44 @@
-/* 
-1,2,3,4 
-i
-1: f{2,3,4}:
-    1, 2, f{3,4}
-    1, 3, f{2,4}
-2, f{1, 3, 4}
+/*
+nums = [1,2,3]
 
-3， f{1,2,4}
+1 2 3
+i 
+1 [2, 3]
+1 2  [3]
+1 2 3
+1 
 
-4, f{1,2,3}
+dfs(cand)
+    // terminate
+    if cand is empty
+        update res
+    for each cand j:
+        curPerm += j
+        swap
+        dfs(cands)
+        swap back
+
 */
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> path;
-        helper(nums, path, res, 0);
+        dfs(0, res, nums);
         return res;
     }
-    
-    // construct a path that is currently at 'layer'.
-    void helper(vector<int>& nums, 
-                vector<int>& path, 
-                vector<vector<int>>& res,
-                int layer) {
+
+    void dfs(int start, vector<vector<int>>& res, vector<int>& nums) {
         // termination
-        int n = nums.size();
-        if (layer ==  n) {
-            res.push_back(path);
+        if (start == nums.size()) {
+            res.push_back(nums);
             return;
         }
-        
+
         // recursion
-        // candidates: layer ~ n -1
-        for (int i = layer; i < n; ++i) {
-            path.push_back(nums[i]); // path: 1
-            swap(nums[i], nums[layer]); // i has been visited
-            helper(nums, path, res, layer + 1); // dfs
-            path.pop_back();
-            swap(nums[i], nums[layer]);
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[i], nums[start]);
+            dfs(start + 1, res, nums);
+            swap(nums[i], nums[start]);
         }
     }
 };
