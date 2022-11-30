077ae967f60e70fba7ca6927ef1a1a06ed4c0293
lawson1225 <liu.sheng@husky.neu.edu>
2022-11-30 22:48:47 +0000

    Sync LeetCode submission - Combination Sum III (cpp)

--- problems/combination_sum_iii/solution.cpp
+++ problems/combination_sum_iii/solution.cpp
@@ -1,20 +1,42 @@
+/*
+1 2 3 4 5 6 7 8 9
+target: 7
+k: 3
+
+dfs(start, path, k, n)
+if n < 0:
+    return
+if k == 0 and n == 0:
+    update res
+    return
+for (i = start; i <= 9; ++i) {
+    path.push_back(i)
+    dfs(i + 1, path, k - 1, n - i)
+    path.pop_out
+}
+*/
 class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> res;
-        vector<int> out;
-        helper(k, n, 1, out, res);
+        vector<int> path;
+        dfs(1, path, k, n, res);
         return res;
     }
-    
-    void helper(int k, int n, int level, vector<int>& out,
-               vector<vector<int>>& res) {
-        if (n < 0) return;
-        if (n == 0 && out.size() == k) res.push_back(out);
-        for (int i = level; i <= 9; ++i) {
-            out.push_back(i);
-            helper(k , n - i, i + 1, out, res);
-            out.pop_back();
+    void dfs(int start, vector<int>& path, int k, int n, vector<vector<int>>& res) {
+        // termination
+        if (n < 0 || k < 0)
+            return;
+        if (n == 0 && k == 0) {
+            res.push_back(path);
+            return;
+        }
+
+        // recursion
+        for (int i = start; i <= 9; ++i) {
+            path.push_back(i);
+            dfs(i + 1, path, k - 1, n - i, res);
+            path.pop_back();
         }
     }
 };
