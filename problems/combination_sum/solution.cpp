5d45b6d2e1d96a0114086828d9e5d68970ca7c75
lawson1225 <liu.sheng@husky.neu.edu>
2022-11-30 04:22:09 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,51 +1,62 @@
 /*
- backtracking
- helper(int idx, cur_sum, target, path, candidates):
-    if (cur_sum == target):
-        res.push_back(path)
+2,3,6,7
+target:7
+
+2  -> 5
+2  -> 3
+2  -> 1 x
+3  -> 0 [2, 2, 3]
+
+tree
+
+                      -
+                2 (5)      3(4)
+              / |   \     /
+           2(3) 3(2)     3(1)
+           /   /
+        2(1)   2(0*)
+
+search problem, dfs
+sort
+f(candidates, path, start, target): starting from start, return all combinitions that sum == target
+    // termination
+    if target == 0:
+        update res
         return
-    if (cur_sum > target):
+    if target <0:
         return
-    
-    // traverse each candidate
-    for (i = idx; i < candidates.size(); ++i):
-        // choose candidates[i]
-        path.push_back(candidates[i])
-        cur_sum += candidates[i]
-        helper(i, cur_sum, target, path, candidates)
-        
-        // no choose candidates[i]
-        path.pop_back()
-        cur_sum -= candiates[i]
+
+    // recurion
+    for each cand "i" from [start, end]:
+        path += cand[i]
+        f(candidates, path, i, target-cand[i])
+        path pop back
 */
+
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
+        vector<vector<int>> res;
         vector<int> path;
-        // sort
         sort(candidates.begin(), candidates.end());
-        helper(0, 0, target, path, candidates, result);
-        return result;
+        dfs(candidates, path, 0, target, res);
+        return res;
     }
-    
-    void helper(int idx, int cur_sum, int target, vector<int>& path, const vector<int>& candidates, vector<vector<int>>& result) {
+
+    void dfs(const vector<int>& candidates, vector<int> path, int start, int target, vector<vector<int>>& res) {
         // termination
-        if (cur_sum == target) {
-            result.push_back(path);
+        if (target == 0) {
+            res.push_back(path);
             return;
         }
-        if (cur_sum > target) {
+        if (target <0) 
             return;
-        }
-        
+
         // recursion
-        for (int i = idx; i < candidates.size(); ++i) {
+        for (int i = start; i < candidates.size(); ++i) {
             path.push_back(candidates[i]);
-            cur_sum += candidates[i];
-            helper(i, cur_sum, target, path, candidates, result);
+            dfs(candidates, path, i, target - candidates[i], res);
             path.pop_back();
-            cur_sum -= candidates[i];
         }
     }
 };
