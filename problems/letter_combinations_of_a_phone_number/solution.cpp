c15294675cc50d26e741171f07398365bf3ee733
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-01 06:02:34 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,52 +1,61 @@
 /*
-2, 3, 4
-2: a, b, c
-3: d, e, f
-4: g, h ,i
+23
+2: a,b,c
+3: d,e,f
+...
+9: w,x,y,z
 
-0(2):          a,       b,        c
-1(3):     d,         e,        f       d, e, f   d, e, f
-2(4): [g, h, i] [g, h, i]  [g, h, i] ....
+ad,ae,af,bd,be,bf,cd,ce,cf
+
+1. hashmap: digit : letters
+2. search dfs
+
+dfs(start, path, res, digits)
+// termination
+if start == digits.end:
+    res += path
+    return
+// recursion
+for each candi i from hashmap[digit]:
+    path.push_back(i)
+    dfs(start + 1, path, res, digits)
+    path.pop_out()
+
 */
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.size() == 0) return {};
-        unordered_map<int, vector<char>> m;
-        m[2] = {'a', 'b', 'c'};
-        m[3] = {'d', 'e', 'f'};
-        m[4] = {'g', 'h', 'i'};
-        m[5] = {'j', 'k', 'l'};
-        m[6] = {'m', 'n', 'o'};
-        m[7] = {'p', 'q', 'r', 's'};
-        m[8] = {'t', 'u', 'v'};
-        m[9] = {'w', 'x', 'y', 'z'};
         vector<string> res;
-        string path = "";
-        helper(res, path, digits, 0, m);
-        return res;
+        string path;
+        // hashmap
+        unordered_map<int, string> m;
+        m[2] = "abc";
+        m[3] = "def";
+        m[4] = "ghi";
+        m[5] = "jkl";
+        m[6] = "mno";
+        m[7] = "pqrs";
+        m[8] = "tuv";
+        m[9] = "wxyz";
+
+        if (digits == "")
+        return {};
         
+        dfs(0, path, res, digits, m);
+        return res;
     }
-    
-    // at current level, constrcut combinations
-    void helper(vector<string> &res, 
-               string path,
-                string digits,
-               int level,
-               unordered_map<int, vector<char>> &m) {
-        // termination
-        int n = digits.size();
-        if (level == n) {
+
+    void dfs(int start, string path, vector<string>& res, const string& digits, unordered_map<int, string>& m) {
+        // terminaiton
+        if (start == digits.size()) {
             res.push_back(path);
             return;
         }
-        
+
         // recursion
-        // at current level, go throught all candi
-        int cur = digits[level] - '0';// 2
-        vector<char> cand = m[cur]; //[a, b, c]
-        for (int i = 0; i < cand.size(); ++i) {
-            helper(res, path + cand[i], digits, level + 1, m); //
+        int cur = digits[start] - '0';  // 2
+        for (char digit : m[cur]) {
+            dfs(start + 1, path + digit, res, digits, m);
         }
     }
 };
