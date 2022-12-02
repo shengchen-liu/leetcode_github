09a2b15f6f5345164d91026541edd33c00237923
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-02 20:32:11 +0000

    Sync LeetCode submission - Binary Search (cpp)

--- problems/binary_search/solution.cpp
+++ problems/binary_search/solution.cpp
@@ -5,12 +5,19 @@ public:
         int end = nums.size() - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] < target) start = mid;
-            else end = mid;
+            if (nums[mid] == target) {
+                start = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
         }
-        if (nums[start] == target) return start;
-        if (nums[end] == target) return end;
-        return -1;
+
+        if (nums[start] == target)
+            return start;
+        if (nums[end] == target)
+            return end;
+        return -1;        
     }
 };
