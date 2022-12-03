c6c9e8129db881a86e705c2458249e53e066d69c
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-03 19:20:51 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,51 +1,40 @@
 /*
-0  1  2  3  4  5  6
-2, 5, 6, 0, 0, 1, 2      l     r    m
-l        m        r      0     6    3
-m = 0 + (6 - 0) / 2= 3
-   *m
- *
-*   
-       *
-     *
-     
-  *   
-* 
-        *
-      *
-    *m
+check rotated in range [start, mid]:
+nums[start] > nums[mid] == true
+mid = start + (end - start) / 2
+
+1. nums[start] <= nums[mid] => no rotated
+    a. nums[start] <= target < nums[mid] < : end = mid
+    b. else: star = mid
+
+2. nums[start] > nums[mid] => rotated
+    a. nums[mid] < target < nums[end] : start = mid
+    b. else: end = mid
+
 */
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        if (nums.size() == 0)
-            return false;
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left < right) {
-            if (nums[left] == nums[right])
-                ++left;
-            else 
-                break;
-        }
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[left] == target || nums[right] == target || nums[mid] == target)
-                return true;
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] < target && target < nums[mid])
-                    right = mid;
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start + 1 < end) {
+            int mid = start  + (end -  start) / 2;
+            if (nums[mid] == target) return true; 
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                ++start;
+                --end;
+            } else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid])
+                    end = mid;
                 else 
-                    left = mid;
+                    start = mid;
             } else {
-                if (nums[mid] < target && target < nums[right])
-                    left = mid;
-                else 
-                    right = mid;
-            } 
+                if (nums[mid] < target && target <= nums[end])
+                    start = mid;
+                else end = mid;
+            }
         }
-                    
-        if (nums[left] == target || nums[right] == target)
+        if (nums[start] == target || nums[end] == target)
             return true;
         return false;
     }
