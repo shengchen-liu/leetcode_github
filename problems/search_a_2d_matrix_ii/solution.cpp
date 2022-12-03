1b2b5657cf59c77cece5bf0edf48adf1b49a7d03
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-03 20:16:56 +0000

    Sync LeetCode submission - Search a 2D Matrix II (cpp)

--- problems/search_a_2d_matrix_ii/solution.cpp
+++ problems/search_a_2d_matrix_ii/solution.cpp
@@ -3,16 +3,13 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        // start from bottom left
+        // bottom left
         int row = m - 1;
         int col = 0;
         while (row >= 0 && col < n) {
-            if (matrix[row][col] == target)
-                return true;
-            if (matrix[row][col] < target)
-                ++col;
-            else 
-                --row;
+            if (matrix[row][col] == target) return true;
+            if (matrix[row][col] < target) col++;
+            else row--;
         }
         return false;
     }
