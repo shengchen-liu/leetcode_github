b87ed064042c98dcb1501d26bc00eef4c0d2a59b
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-03 16:50:50 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- problems/search_in_rotated_sorted_array/solution.cpp
+++ problems/search_in_rotated_sorted_array/solution.cpp
@@ -1,31 +1,52 @@
 /*
-if nums[mid] == target => return mid
-if nums[start] < nums[mid]: 
-    1) nums[start] <= target <= nums[mid] => end = mid
-    2) else: start = mid
-if nums[start] < nums[mid]:
-    1) nums[mid] <= target <= nums[end] => start = mid
-    2) else: end = mid
+nums = [4,5,6,7,0,1,2], target=0.  return 4
+    
+      7
+    6
+  5
+              3
+            2
+          1
+        0
+
+check rotated in range [start, end]:
+nums[start] > nums[end] == true
+mid = start + (end - start) / 2
+
+1. nums[start] < nums[mid] => no rotated
+    a. nums[start] < target < nums[mid] < : end = mid
+    b. else: star = mid
+
+2. nums[start] > nums[mid] => rotated
+    a. nums[mid] < target < nums[end] : start = mid
+    b. else: end = mid
 */
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size() == 0) return -1;
+        int n = nums.size();
         int start = 0;
-        int end = nums.size() - 1;
+        int end = n - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) return mid;
             if (nums[start] <= nums[mid]) {
-                if (nums[start] <= target && target < nums[mid]) end = mid;
-                else start = mid;
+                if (nums[start] <= target && target < nums[mid])
+                    end = mid;
+                else 
+                    start = mid;
             } else {
-                if (nums[mid] < target && target <= nums[end]) start = mid;
-                else end = mid;
+                if (nums[mid] < target && target <= nums[end])
+                    start = mid;
+                else 
+                    end = mid;
             }
         }
-        if (nums[start] == target) return start;
-        else if (nums[end] == target) return end;
-        else return -1;
+
+        if (nums[start] == target)
+            return start;
+        if (nums[end] == target)
+            return end;
+        return -1;
     }
 };
