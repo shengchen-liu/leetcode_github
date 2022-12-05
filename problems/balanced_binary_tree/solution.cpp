6cbf48277451c5a6dd7e09be89be76d5999b1351
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-05 01:56:38 +0000

    Sync LeetCode submission - Balanced Binary Tree (cpp)

--- problems/balanced_binary_tree/solution.cpp
+++ problems/balanced_binary_tree/solution.cpp
@@ -4,17 +4,11 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-/*
-       node
-heightL    heightR
 
-if abs(heightL - heightR) <= 1 => f(node) == true, height of node: 1 + max(hL, hR)
-*/
+
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
