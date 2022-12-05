3d106764f5780693592db2d6826e49a14222b2cd
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-05 23:32:32 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (cpp)

--- problems/binary_tree_level_order_traversal/solution.cpp
+++ problems/binary_tree_level_order_traversal/solution.cpp
@@ -9,32 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ /*
+ queue
+ while (!queue.empty):
+    for all nodes in the queue: (size is fixed)
+        node = quee.front
+        queue.pop
+        for each node, store its left & right childs into the queue
+        add node.value into the layer
+    update the res by layer
+
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        //BFS
-        queue<TreeNode*> q;
         if (!root)
             return res;
+        queue<TreeNode*> q;
         q.push(root);
-        while (!q.empty()) {
-            // layer
+        while(!q.empty()) {
             int size = q.size();
-            vector<int> tmp;
+            vector<int> layer;
             for (int i = 0; i < size; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
-                tmp.push_back(node->val);
-                // childs
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right){
-                    q.push(node->right);
-                }
+                layer.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-            res.push_back(tmp);
+            res.push_back(layer);
         }
         return res;
     }
