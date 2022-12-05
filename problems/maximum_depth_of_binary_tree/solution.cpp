7244835afff4f2542c2c77f4d073dffa4055cb8b
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-05 01:41:52 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- problems/maximum_depth_of_binary_tree/solution.cpp
+++ problems/maximum_depth_of_binary_tree/solution.cpp
@@ -9,23 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ /*
+ search 
+ dfs, keep track of layer, max_depth
+ if reaches to leaf, update max_depth with layer
+ 
+pre order traverse
+dfs(node, level, res)  : starting at cur node, at this level, find max_depth
+    if !node:
+        return
+    if node is a leaf:
+        res = max(level, res)
+        return
+    dfs(node->left, level + 1, res)
+    dfs(node->right, level+1, res)
+ */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         int res = 0;
-        res = dfs(root);
+        dfs(root, 1, res);
         return res;
     }
-    
-    // maxDepth starting from node
-    int dfs(TreeNode* node) {
-        // termination
-        if (!node) return 0;
-        
-        // recursion
-        int tmp = max(dfs(node->left), dfs(node->right)) + 1;
-                
-        return tmp;
-        
+
+    void dfs(TreeNode* node, int level, int& res) {
+        if (!node) return;
+        if (!node->left && !node->right) {
+            res = max(res, level);
+            return;
+        }
+        dfs(node->left, level + 1, res);
+        dfs(node->right, level + 1, res);
     }
 };
