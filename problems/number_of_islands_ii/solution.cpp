fa77e474a28aa5da7c0623f0ee648a8c1050c6f9
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-13 23:25:44 +0000

    Sync LeetCode submission - Number of Islands II (cpp)

--- problems/number_of_islands_ii/solution.cpp
+++ problems/number_of_islands_ii/solution.cpp
@@ -1,23 +1,65 @@
+/*
+m = 3, n = 3
+
+0 0 0
+0 0 0
+0 0 0 
+
+[0,0] => 1
+1 0 0
+0 0 0
+0 0 0 
+
+[0,1] => 1
+1 1 0
+0 0 0
+0 0 0 
+
+[1,2] => 2 
+1 1 0
+0 0 1
+0 0 0 
+
+union find
+every operation:
+build a new group[i,j]
+    father({i,j}) = {i,j}
+    ++cnt
+for each of its 4 neighbors:
+    if neighbor == 1:
+        merge node's group with neighbor's group
+        --cnt
+
+return total number of groups
+*/
 class Solution {
 public:
     vector<int> numIslands2(int m, int n, vector<vector<int>>& positions) {
         vector<int> res;
         int cnt = 0;
-        vector<int> roots(m * n, -1);
-        vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-        for (auto &pos : positions) {
-            int id = n * pos[0] + pos[1];
-            if (roots[id] != -1) {
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
+        vector<int> roots(m * n, -1);  
+
+        for (auto position : positions) {
+            int x = position[0];
+            int y = position[1];
+            int idx = n * x + y;
+            if (roots[idx] != -1) {
                 res.push_back(cnt);
                 continue;
             }
-            roots[id] = id;
+            roots[idx] = idx;
             ++cnt;
-            for (auto dir : dirs) {
-                int x = pos[0] + dir[0], y = pos[1] + dir[1], cur_id = n * x + y;
-                if (x < 0 || x >= m || y < 0 || y >= n || roots[cur_id] == -1) continue;
-                int p = findRoot(roots, cur_id), q = findRoot(roots, id);
-                if (p != q) {
+            for (int i = 0; i < 4; ++i) {
+                int x1 = x + dx[i];
+                int y1 = y + dy[i];
+                int cur_id = n * x1 + y1;
+                if (x1 < 0 || x1 > m - 1 || y1 < 0 || y1 > n - 1 || roots[cur_id] == -1)
+                    continue;
+                int p = findRoot(roots, cur_id);
+                int q = findRoot(roots, idx);
+                if (p != q){
                     roots[p] = q;
                     --cnt;
                 }
@@ -26,7 +68,11 @@ public:
         }
         return res;
     }
+
     int findRoot(vector<int>& roots, int id) {
-        return (id == roots[id]) ? id : findRoot(roots, roots[id]);
+        if (id == roots[id])
+            return id;
+        roots[id] = findRoot(roots, roots[id]);
+        return roots[id];
     }
 };
