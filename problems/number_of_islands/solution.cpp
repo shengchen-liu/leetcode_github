a0087ee82d1432b2e0d5e2429676f92bddbda548
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-13 18:58:07 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,44 +1,30 @@
-/*
-dfs
-*/
-
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int cnt = 0;
-        if (grid.size() == 0)
-            return cnt;
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
+
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (visited[i][j] || grid[i][j] == '0')
-                    continue;
-                dfs(i, j, grid, visited);
+                if (visited[i][j] || grid[i][j] == '0') continue;
+                dfs(grid, i, j, visited);
                 ++cnt;
             }
         }
-        
         return cnt;
     }
-    
-    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-        // termination
+
+    void dfs(vector<vector<char>>& grid, int x, int y, vector<vector<bool>> &visited) {
         int m = grid.size();
         int n = grid[0].size();
-        
-        if (i < 0 || i >= m || j <0 || j >= n || visited[i][j] || grid[i][j] == '0')
+        if (x < 0 || x > m - 1 || y < 0 || y > n - 1 || visited[x][y] || grid[x][y] == '0')
             return;
-        
-        // recursion
-        visited[i][j] = true;
-        
-        // four directions
-        dfs(i - 1, j, grid, visited);
-        dfs(i + 1, j, grid, visited);
-        dfs(i, j - 1, grid, visited);
-        dfs(i, j + 1, grid, visited);
+        visited[x][y] = true;
+        dfs(grid, x + 1, y, visited);
+        dfs(grid, x - 1, y, visited);
+        dfs(grid, x, y + 1, visited);
+        dfs(grid, x, y - 1, visited);
     }
 };
