45e6b427ef83e26430aeb1400a93e299a0e62a00
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-20 03:30:18 +0000

    Sync LeetCode submission - Palindrome Linked List (cpp)

--- problems/palindrome_linked_list/solution.cpp
+++ problems/palindrome_linked_list/solution.cpp
@@ -3,30 +3,64 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
+ */
+ /*
+ reverse 2nd half
+
+ 1 -> 2 -> 2 -> 1
+ i
+ j
+      i
+           j
+
+
+1 -> 2 -> 2 -> 2 -> 1
+ij
+     i    j
+          i        j
+
+               q
+
+1 -> 2 -> 2 -> 2 <- 1
+p                   q
+         
+
  */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if (!head || !head->next) return true;
-        ListNode *slow = head, *fast = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* first = head;
+        // find 2nd half
         while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode *last = slow->next, *pre = head;
-        while (last->next) {
-            ListNode *tmp = last->next;
-            last->next = tmp->next;
-            tmp->next = slow->next;
-            slow->next = tmp;
-        }
-        while (slow->next) {
-            slow = slow->next;
-            if (pre->val != slow->val) return false;
-            pre = pre->next;
+        ListNode* secondHalf = reverseList(slow->next);
+        ListNode* firstHalf = head;
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) return false;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
         }
         return true;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 };
