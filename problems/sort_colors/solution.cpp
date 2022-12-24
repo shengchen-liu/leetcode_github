7d1b9c7149d30552a8312a02cc66c4173c962293
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-24 02:11:43 +0000

    Sync LeetCode submission - Sort Colors (cpp)

--- problems/sort_colors/solution.cpp
+++ problems/sort_colors/solution.cpp
@@ -1,43 +1,36 @@
 /*
+2,0,2,1,1,0 
+i, j
+i: all nums before i are 0
+j: all nums after j are 2
+
 2, 0, 2, 1, 1, 0
-ik             j
+i  k           j
+0, 2, 2, 1, 1, 0
+   i           k
 0, 0, 2, 1, 1, 2
-ik          j
-0, 0, 2, 1, 1, 2
-   ik       j
-0, 0, 2, 1, 1, 2
-      ik    j
-0, 0, 1, 1, 2, 2
-      ik j
-         k
-   
-all values before i == 0
-all values after j == 2
+               kj
 
-nums[k] == 2:
-    swap (nums[j], nums[k])
-    --j
-nums[k] == 0:
-    swap (nums[i], nums[k])
-    ++i，++k
-nums[k] == 1:
-    ++k
-
+k from i to end:
+    if nums[k] == 0: swap(nums[i], nums[k]), ++i
+    else: ++k
+k from j to 0:
+    if nums[k] == 2: swap(nums[j], nums[k]), --i
+    else: --k
 */
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
         int i = 0;
-        int j = n - 1;
+        int j = nums.size() - 1;
         int k = 0;
         while (k <= j) {
             if (nums[k] == 0) {
                 swap(nums[i++], nums[k++]);
             } else if (nums[k] == 2){
                 swap(nums[j--], nums[k]);
-            }
-            else k++;
+            } else
+                ++k;
         }
     }
 };
