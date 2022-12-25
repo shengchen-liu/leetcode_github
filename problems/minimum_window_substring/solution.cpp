64b308d00f23c5b9692293b11a14f348a77206fa
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-25 01:52:42 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,57 +1,20 @@
-/*
-s = "ADOBECODEBANC", t = "ABC"
-hashmap:
-A: 1
-B: 1
-C: 1
-ADOBECODEBANC     map           cnt    
-ij                A:0 B:1 C:1   1 
-i j               A:0 B:1 C:1   1
-i  j              A:0 B:0 C:1   2
-i   j             A:0 B:0 C:1   2
-i    j            A:0 B:0 C:0   3    move left, update min_len
- i   j            A:1 B:0 C:0   2    move     
- i       j        A:0 B:0 C:0   3    move left
-  i      j        A:0 B:0 C:0
-   i  j 
-   i   j
-   i    j
-   i     j
-   i      j                 B:2, C:1
-   i       j                A:1, B:2, C:1
-*/
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string res = "";
-        unordered_map<char, int> letterCnt;
-        for (char c : t) ++letterCnt[c];
-        unordered_map<char, int>  sourceCnt;
-        int j = 0;
-        int cnt = 0; // num. of letters that have same frequences with target
-        int minLen = INT_MAX;
-        for (int i = 0; i < s.size(); ++i) {
-            while (j < s.size() && cnt < letterCnt.size()) {
-                if (letterCnt.count(s[j])) {
-                    sourceCnt[s[j]]++;
-                    if (sourceCnt[s[j]] == letterCnt[s[j]])
-                        ++cnt;
-                }
-                ++j;
-            }
-            
-            if (j - i < minLen && cnt == letterCnt.size()){
-                minLen = j - i;
-                res = s.substr(i, minLen);
-            }
-            
-            // remove i and move forward
-            if (letterCnt.count(s[i])) {
-                if (letterCnt[s[i]] == sourceCnt[s[i]])
-                    --cnt;
-                sourceCnt[s[i]]--;
+        unordered_map<char, int> letters; //unordered map for storing the characters in t that we need to check for in s
+        for(auto c : t) letters[c]++; 
+        int count = 0; //counts number of t's letters in current window
+        int low = 0, min_length = INT_MAX, min_start = 0;     
+        for(int high = 0; high<s.length(); high++) {
+            if(letters[s[high]] > 0) count++; //means that this letter is in t   
+            letters[s[high]]--; //reduce the count for the letter on which we are currently 
+            if(count == t.length()) { //if current windows contains all of the letters in t
+                while(low < high && letters[s[low]] < 0) letters[s[low]]++, low++; //move low ahead if its not of any significance
+                if(min_length > high-low) min_length = high-(min_start=low)+1; //update the min length
+                letters[s[low++]]++; //move low ahaead and also increment the value
+                count--; //count-- as we are moving low ahead & low pointed to a char in t before
             }
         }
-        return res;
+        return min_length == INT_MAX ? "" : s.substr(min_start, min_length); //check for edge case & return the result
     }
 };
