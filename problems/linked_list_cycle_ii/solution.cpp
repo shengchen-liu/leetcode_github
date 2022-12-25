f39fd05c332f0271dd071dcc98838bb95c847716
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-25 05:50:35 +0000

    Sync LeetCode submission - Linked List Cycle II (cpp)

--- problems/linked_list_cycle_ii/solution.cpp
+++ problems/linked_list_cycle_ii/solution.cpp
@@ -9,15 +9,14 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head) return NULL;
-        ListNode *slow = head;
-        ListNode *fast = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) {// there is a circle
-                ListNode *p1 = slow;
-                ListNode *p2 = head;
+            if (slow == fast) {
+                ListNode* p1 = head;
+                ListNode* p2 = slow;
                 while (p1 != p2) {
                     p1 = p1->next;
                     p2 = p2->next;
