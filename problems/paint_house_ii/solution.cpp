b003427ae1ab45f4ced0c42728c1b85991e51705
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-29 23:54:20 +0000

    Sync LeetCode submission - Paint House II (cpp)

--- problems/paint_house_ii/solution.cpp
+++ problems/paint_house_ii/solution.cpp
@@ -1,18 +1,44 @@
+/*
+1,5,3
+2,9,4
+
+dp[i][j]: min cost to paint first i houses, ith house has color j
+dp[i][j] = costs[i-1][j] + min(dp[i-1][r], r in (0 to k exept j))
+*/
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
-        if (costs.empty()) return 0;
-        int n = costs.size(), k = costs[0].size(), m1 = 0, m2 = 0;
-        vector<int> dp(k, 0);
-        for (int i = 0; i < n; i++) {
-            int t1 = m1, t2 = m2;
-            m1 = m2 = INT_MAX;
-            for (int j = 0; j < k; j++) {
-                dp[j] = (dp[j] != t1 ? t1 : t2) + costs[i][j];
-                if (m1 <= dp[j]) m2 = min(m2, dp[j]);
-                else m2 = m1, m1 = dp[j];
+        int n = costs.size();
+        int k = costs[0].size();
+        vector<int> previousRow(k, 0);
+        vector<int> currentRow(k, 0);
+
+        for (int i = 1; i <= n; ++i) {
+            // Find the minimum and second minimum color in the PREVIOUS row.
+            int minColor = -1; int secondMinColor = -1;
+            for (int color = 0; color < k; color++) {
+                int cost = previousRow[color];
+                if (minColor == -1 || cost < previousRow[minColor]) {
+                    secondMinColor = minColor;
+                    minColor = color;
+                } else if (secondMinColor == -1 || cost < previousRow[secondMinColor]) {
+                    secondMinColor = color;
+                }
             }
+
+            // And now calculate the new costs for the current row.
+            for (int color = 0; color < k; ++color) {
+                if (color == minColor)
+                    currentRow[color] = previousRow[secondMinColor] + costs[i - 1][color];
+                else 
+                    currentRow[color] = previousRow[minColor] + costs[i - 1][color];
+            }
+            previousRow = currentRow;
         }
-        return m1;
+        int res = INT_MAX;
+        for (int i = 0; i < k; ++i) {
+            res = min(res, currentRow[i]);
+        }
+        return res;
     }
-}; 
+};
