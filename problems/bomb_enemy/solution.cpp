22e45d7592a669b433dd576701e9a17eca604f19
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-29 07:23:37 +0000

    Sync LeetCode submission - Bomb Enemy (cpp)

--- problems/bomb_enemy/solution.cpp
+++ problems/bomb_enemy/solution.cpp
@@ -1,33 +1,45 @@
+/*
+"0","E","0","0"
+"E","0","W","E"
+"0","E","0","0"
+*/
 class Solution {
 public:
     int maxKilledEnemies(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), res = 0;
-        vector<vector<int>> v1(m, vector<int>(n, 0)), v2 = v1, v3 = v1, v4 = v1;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int t = (j == 0 || grid[i][j] == 'W') ? 0 : v1[i][j - 1];
-                v1[i][j] = grid[i][j] == 'E' ? t + 1 : t;
-            }
-            for (int j = n - 1; j >= 0; --j) {
-                int t = (j == n - 1 || grid[i][j] == 'W') ? 0 : v2[i][j + 1];
-                v2[i][j] = grid[i][j] == 'E' ? t + 1 : t;
-            }
-        }
-        for (int j = 0; j < n; ++j) {
-            for (int i = 0; i < m; ++i) {
-                int t = (i == 0 || grid[i][j] == 'W') ? 0 : v3[i - 1][j];
-                v3[i][j] = grid[i][j] == 'E' ? t + 1 : t;
-            }
-            for (int i = m - 1; i >= 0; --i) {
-                int t = (i == m - 1 || grid[i][j] == 'W') ? 0 : v4[i + 1][j];
-                v4[i][j] = grid[i][j] == 'E' ? t + 1 : t;
-            }
-        }
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == '0') {
-                    res = max(res, v1[i][j] + v2[i][j] + v3[i][j] + v4[i][j]);
+        int m = grid.size();
+        int n = grid[0].size();
+        int rowHits = 0;
+        vector<int> dp_col(n, 0);
+        int res = 0;
+
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                // start from col to end of the row, update rowHits
+                if (col == 0 || grid[row][col - 1] == 'W') {
+                    rowHits = 0;
+                    for (int k = col; k < n; ++k) {
+                        if (grid[row][k] == 'W')
+                            break;  // stop the scan when we hit the wall
+                        else if (grid[row][k] == 'E')
+                            rowHits++;
+                    }
+                }
+
+                // start from row to end of the column, update colHits
+                if (row == 0 || grid[row - 1][col] == 'W') {
+                    int colHits = 0;
+                    for (int k = row; k < m; ++k) {
+                        if (grid[k][col] == 'W') 
+                            break;
+                        else if (grid[k][col] == 'E')
+                            colHits++;
+                    }
+                    dp_col[col] = colHits;
+                }
+
+                // empty cell
+                if (grid[row][col] == '0') {
+                    res = max(res, rowHits + dp_col[col]);
                 }
             }
         }
