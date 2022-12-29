3ce322e88f929cae0e4cbb201ec746ea59f29ea7
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-29 09:17:13 +0000

    Sync LeetCode submission - Maximal Square (cpp)

--- problems/maximal_square/solution.cpp
+++ problems/maximal_square/solution.cpp
@@ -1,21 +1,31 @@
+/*
+dp[i][j]: start from (i, j) as bottom right corner, max square area
+dp[0][0] = 0 if matrix[i][j] == 0 
+         = 1  if matrix[0][0] == 0
+
+dp[i][j] vs dp[i-1][j-1]:
+    if matrix[i][j] == 0 : dp[i][j] = 0
+    else:
+        if matrix[i][j-1] == 1 and matrix[i-1][j] == 1: dp[i][j] = dp[i-1][j-1] + 1
+*/
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
-        int m = matrix.size(), n = matrix[0].size(), res = 0, pre = 0;
-        vector<int> dp(m + 1, 0);
-        for (int j = 0; j < n; ++j) {
-            for (int i = 1; i <= m; ++i) {
-                int t = dp[i];
-                if (matrix[i - 1][j] == '1') {
-                    dp[i] = min(dp[i], min(dp[i - 1], pre)) + 1;
-                    res = max(res, dp[i]);
-                } else {
-                    dp[i] = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int res = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m ; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                } else if (matrix[i][j] == '1') {
+                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                 }
-                pre = t;
+                res = max(res, dp[i][j]);
             }
         }
+
         return res * res;
     }
 };
