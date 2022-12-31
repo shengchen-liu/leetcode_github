53e8b59452358be2e14d2750c2a9b4c56ca51108
lawson1225 <liu.sheng@husky.neu.edu>
2022-12-31 00:43:35 +0000

    Sync LeetCode submission - Decode String (cpp)

--- problems/decode_string/solution.cpp
+++ problems/decode_string/solution.cpp
@@ -1,39 +1,36 @@
 /*
-3[a2[c[c]]]
-repeat = 3
-content = dfs(a2[c[c]])
-content,content,content
-
-1. char
-2. num
-
+stack
+1) [ : begin sth
+2) ] : end of sth.  start to pop stack until reach to [  => [....] 
+3) digit: updat the cnt
 */
 class Solution {
 public:
     string decodeString(string s) {
-        int index = 0;
-        return decodeString(s, index);
-    }
-    
-    string decodeString(const string& s, int& index) {
-        string result;
-        while (index < s.length() && s[index] != ']') {
-            if (!isdigit(s[index]))
-                result += s[index++];
-            else {
-                int k = 0;
-                // build k while next character is a digit
-                while (index < s.length() && isdigit(s[index]))
-                    k = k * 10 + s[index++] - '0';  
-                // ignore the opening bracket '['    
-                index++;
-                string decodedString = decodeString(s, index);
-                // ignore the closing bracket ']'
-                index++;        
-                while (k-- > 0)
-                    result += decodedString;
+        string t = "";
+        stack<int> s_cnt;
+        stack<string> s_str;
+        int cnt = 0;
+        for (auto c : s) {
+            if (c >= '0' && c <= '9') {
+                cnt = cnt * 10 + (c - '0') ;
+            } else if (c == '[') {
+                s_cnt.push(cnt);
+                s_str.push(t);
+                cnt = 0;
+                t = "";
+            } else if (c == ']') {
+                int k = s_cnt.top();
+                s_cnt.pop();
+                for (int i = 0; i < k; ++i) {
+                    s_str.top() += t;
+                }
+                t = s_str.top();
+                s_str.pop();
+            } else {
+                t += c;
             }
         }
-        return result;
+        return s_str.empty()? t : s_str.top();
     }
 };
