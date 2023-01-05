0a91046e80f88767852a02d7636f7052ce9b2bc6
lawson1225 <liu.sheng@husky.neu.edu>
2023-01-05 05:54:01 +0000

    Sync LeetCode submission - First Missing Positive (cpp)

--- problems/first_missing_positive/solution.cpp
+++ problems/first_missing_positive/solution.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        // 1. check if 1 is present
         int n = nums.size();
-        bool num_1 = false;
-        
-        for (int num : nums) {
-            if (num == 1) {
-                num_1 = true;
-                break;
-            }
-        }
-        
-        if (!num_1) return 1;
-        
-        // 2. [1]
-        if (n == 1) return 2;
-        
-        // 3. replace non-pos values and values > n  to be 1s
+        // change num < 1 to 0
         for (int i = 0; i < n; ++i) {
-            if (nums[i] <= 0 || nums[i] > n ) {
-                nums[i] = 1;
-            }
+            if (nums[i] < 0)
+                nums[i] = 0;
         }
-        
-        // 4. for val = nums[i], set the nums[val] to be negative
+
+        // if val = nums[i] is in range of [1, n], mark nums[val-1] as negative
         for (int i = 0; i < n; ++i) {
-            int val =  abs(nums[i]);
-            // check nums[abs(val)]
-            
-            if (val == n) {
-                nums[0] = -abs(nums[0]);
-            }
-            else {
-                nums[val] = -abs(nums[val]);
+            int val = abs(nums[i]);
+            if (val > 0 && val <= n) {
+                if (nums[val - 1] > 0)
+                    nums[val - 1] *= -1; 
+                else if (nums[val - 1] == 0)
+                    nums[val - 1] = -1 * (n + 1);
             }
         }
         
-        // 5. find the 1st positive value
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > 0) return i;
+        for (int i = 1; i <= n; ++i) {
+            if (nums[i - 1] >= 0)
+                return i;
         }
-        
-        if (nums[0] > 0) return n;
-        
         return n + 1;
-        
     }
 };
