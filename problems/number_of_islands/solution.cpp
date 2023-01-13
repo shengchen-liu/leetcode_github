3c12d0dab78184fe6e445f52b9d3072a5e2c36bd
lawson1225 <liu.sheng@husky.neu.edu>
2023-01-13 02:39:37 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int cnt = 0;
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-
+        int cnt = 0;
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (visited[i][j] || grid[i][j] == '0') continue;
-                dfs(grid, i, j, visited);
+            for (int j  = 0; j < n; ++j) {
+                if (visited[i][j] || grid[i][j] == '0')
+                    continue;
+                dfs(i, j, visited, grid);
                 ++cnt;
             }
         }
         return cnt;
     }
 
-    void dfs(vector<vector<char>>& grid, int x, int y, vector<vector<bool>> &visited) {
+    void dfs(int i, int j, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        if (x < 0 || x > m - 1 || y < 0 || y > n - 1 || visited[x][y] || grid[x][y] == '0')
+        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] == '0')
             return;
-        visited[x][y] = true;
-        dfs(grid, x + 1, y, visited);
-        dfs(grid, x - 1, y, visited);
-        dfs(grid, x, y + 1, visited);
-        dfs(grid, x, y - 1, visited);
+        visited[i][j] = true;
+        dfs(i + 1, j, visited, grid);
+        dfs(i, j + 1, visited, grid);
+        dfs(i - 1, j, visited, grid);
+        dfs(i, j - 1, visited, grid);
     }
 };
