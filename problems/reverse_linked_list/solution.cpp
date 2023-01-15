23236cc57a408620b57e80ed707df04797765717
lawson1225 <liu.sheng@husky.neu.edu>
2023-01-15 22:53:56 +0000

    Sync LeetCode submission - Reverse Linked List (cpp)

--- problems/reverse_linked_list/solution.cpp
+++ problems/reverse_linked_list/solution.cpp
@@ -11,7 +11,8 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (!head || !head->next) return head;
+        if (!head || !head->next)
+            return head;
         ListNode* newHead = reverseList(head->next);
         head->next->next = head;
         head->next = NULL;
