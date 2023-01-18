31f3fbb6b911889e87d0d90fb1d1d65207297fc5
lawson1225 <liu.sheng@husky.neu.edu>
2023-01-18 08:01:41 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (cpp)

--- problems/maximum_sum_circular_subarray/solution.cpp
+++ problems/maximum_sum_circular_subarray/solution.cpp
@@ -1,14 +1,35 @@
+/*
+case 1
+regular array: 
+dp[i]: max sum for left ith nums, have to use ith num
+1) only thi num.  nums[i-1]
+2) use previous num.  dp[i - 1] + nums[i - 1]
+dp[i] = max(nums[i-1], dp[i-1]+nums[i-1])
+
+case2: the subarary take a part of both sides
+convert it to : max res = total sum -  min subarray sum in normal arrary
+
+*/
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& A) {
-        int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
-        for (int a : A) {
-            curMax = max(curMax + a, a);
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int curMax = 0;
+        int curMin = 0;
+        int maxSum = INT_MIN;
+        int minSum = INT_MAX;
+        int totalSum = 0;
+        for (int num : nums)
+            totalSum += num;
+        for (int num : nums) {
+            // case 1
+            curMax = max(curMax + num, num);
             maxSum = max(maxSum, curMax);
-            curMin = min(curMin + a, a);
+
+            // case 2, minSum
+            curMin = min(curMin + num, num);
             minSum = min(minSum, curMin);
-            total += a;
         }
-        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
+
+        return maxSum > 0 ? max(maxSum, totalSum - minSum) : maxSum;
     }
 };
