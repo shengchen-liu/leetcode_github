b67153d8164f90a223451dbf3853d3c636050f3f
lawson1225 <liu.sheng@husky.neu.edu>
2023-03-02 08:52:12 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (cpp)

--- problems/implement_trie_(prefix_tree)/solution.cpp
+++ problems/implement_trie_(prefix_tree)/solution.cpp
@@ -9,34 +9,32 @@ public:
 
 class Trie {
 public:
-    /** Initialize your data structure here. */
     Trie() {
-        root = new TrieNode();        
+        root = new TrieNode();
     }
     
-    /** Inserts a word into the trie. */
     void insert(string word) {
         TrieNode *p = root;
         for (auto &a : word) {
             int i = a - 'a';
-            if (!p->child[i]) p->child[i] = new TrieNode();
+            if (!p->child[i])
+                p->child[i] = new TrieNode();
             p = p->child[i];
         }
         p->isWord = true;
     }
     
-    /** Returns if the word is in the trie. */
     bool search(string word) {
         TrieNode *p = root;
         for (auto &a : word) {
             int i = a - 'a';
-            if (!p->child[i]) return false;
+            if (!p->child[i])
+                return false;
             p = p->child[i];
         }
         return p->isWord;
     }
     
-    /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
         TrieNode *p = root;
         for (auto &a : prefix) {
@@ -46,8 +44,8 @@ public:
         }
         return true;
     }
-    
-private: 
+
+private:
     TrieNode* root;
 };
 
